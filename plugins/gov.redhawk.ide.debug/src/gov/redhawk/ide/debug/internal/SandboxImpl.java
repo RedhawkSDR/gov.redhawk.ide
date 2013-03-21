e27654da7047db1ab6c23878ec416114527a7bb2
Erik Englund <erik.englund@axiosengineering.com>
2013-03-21 16:11:22 -0400

    refs #500 remove Resource Type from Resource Descriptor. Fix the profile method of the resource factory. Simply return the profile from the descriptor.

--- plugins/gov.redhawk.ide.debug/src/gov/redhawk/ide/debug/internal/SandboxImpl.java
+++ plugins/gov.redhawk.ide.debug/src/gov/redhawk/ide/debug/internal/SandboxImpl.java
@@ -66,7 +66,7 @@ public class SandboxImpl implements SandboxOperations {
 		final ResourceDesc[] resources = registry.getResources();
 		final List<String> retVal = new ArrayList<String>(resources.length);
 		for (final ResourceDesc desc : resources) {
-			final String path = registry.getProfilePath(desc);
+			final String path = desc.getProfile();
 			retVal.add(path);
 		}
 		return retVal.toArray(new String[retVal.size()]);
@@ -85,19 +85,19 @@ public class SandboxImpl implements SandboxOperations {
 
 		for (final ScaComponent cp : this.localSca.getSandboxWaveform().getComponents()) {
 			final gov.redhawk.ExtendedCF.ResourceDesc desc = new gov.redhawk.ExtendedCF.ResourceDesc();
-			desc.profile = registry.getProfilePath(registry.getDescByID(cp.getProfileObj().getId()));
+			desc.profile = registry.getDescByID(cp.getProfileObj().getId()).getProfile();
 			desc.resource = cp.getObj();
 			retVal.add(desc);
 		}
 		for (final ScaDevice< ? > cp : this.localSca.getSandboxDeviceManager().getAllDevices()) {
 			final gov.redhawk.ExtendedCF.ResourceDesc desc = new gov.redhawk.ExtendedCF.ResourceDesc();
-			desc.profile = registry.getProfilePath(registry.getDescByID(cp.getProfileObj().getId()));
+			desc.profile = registry.getDescByID(cp.getProfileObj().getId()).getProfile();
 			desc.resource = cp.getObj();
 			retVal.add(desc);
 		}
 		for (final ScaWaveform cp : this.localSca.getWaveforms()) {
 			final gov.redhawk.ExtendedCF.ResourceDesc desc = new gov.redhawk.ExtendedCF.ResourceDesc();
-			desc.profile = registry.getProfilePath(registry.getDescByID(cp.getProfileObj().getId()));
+			desc.profile = registry.getDescByID(cp.getProfileObj().getId()).getProfile();
 			desc.resource = cp.getObj();
 			retVal.add(desc);
 		}
