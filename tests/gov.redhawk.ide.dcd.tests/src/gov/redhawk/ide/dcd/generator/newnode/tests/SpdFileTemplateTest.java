89954852f6c729172f9792b0d0576f457d0599f1
Erik Englund <erik.englund@axiosengineering.com>
2013-04-16 18:50:06 -0400

    Add jacorb dependency

--- tests/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newnode/tests/SpdFileTemplateTest.java
+++ tests/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newnode/tests/SpdFileTemplateTest.java
@@ -43,7 +43,8 @@ public class SpdFileTemplateTest {
 		final SpdFileTemplate spdTemplate = SpdFileTemplate.create(null);
 		final GeneratorArgs args = new GeneratorArgs();
 		args.setAuthorName("MyName");
-		args.setNodeId("MyIdentifier");
+		args.setNodeId("MyNodeIdentifier");
+		args.setSoftPkgId("mySpdId");
 		args.setProjectName("MyProject");
 		final String spdContent = spdTemplate.generate(args);
 
@@ -54,7 +55,7 @@ public class SpdFileTemplateTest {
 		final ResourceSet resourceSet = new ResourceSetImpl();
 		final SoftPkg softpkg = SoftPkg.Util.getSoftPkg(resourceSet.getResource(URI.createFileURI(spdFile.toString()), true));
 		Assert.assertEquals(args.getProjectName(), softpkg.getName());
-		Assert.assertEquals(args.getNodeId(), softpkg.getId());
+		Assert.assertEquals(args.getSoftPkgId(), softpkg.getId());
 		Assert.assertEquals(args.getAuthorName(), softpkg.getAuthor().get(0).getName().get(0));
 		Assert.assertEquals("nodeBooter", softpkg.getImplementation().get(0).getCode().getLocalFile().getName());
 	}
