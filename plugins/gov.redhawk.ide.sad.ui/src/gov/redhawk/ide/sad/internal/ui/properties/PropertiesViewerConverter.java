96dd9506c0d5bd4a769e7e51f0e2084af7e03e97
Erik Englund <erik.englund@axiosengineering.com>
2013-07-16 12:22:32 -0400

    Added controls for changing the SAD values.

--- plugins/gov.redhawk.ide.sad.ui/src/gov/redhawk/ide/sad/internal/ui/properties/PropertiesViewerConverter.java
+++ plugins/gov.redhawk.ide.sad.ui/src/gov/redhawk/ide/sad/internal/ui/properties/PropertiesViewerConverter.java
@@ -10,11 +10,26 @@
  *******************************************************************************/
 package gov.redhawk.ide.sad.internal.ui.properties;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import gov.redhawk.ide.sad.internal.ui.properties.model.ViewerComponent;
 import gov.redhawk.ide.sad.internal.ui.properties.model.ViewerProperty;
+import gov.redhawk.ide.sad.internal.ui.properties.model.ViewerSequenceProperty;
 import gov.redhawk.ide.sad.internal.ui.properties.model.ViewerSimpleProperty;
 import gov.redhawk.sca.util.PluginUtil;
 import mil.jpeojtrs.sca.partitioning.ComponentProperties;
+import mil.jpeojtrs.sca.partitioning.PartitioningPackage;
+import mil.jpeojtrs.sca.prf.AbstractProperty;
+import mil.jpeojtrs.sca.prf.AbstractPropertyRef;
+import mil.jpeojtrs.sca.prf.PrfFactory;
+import mil.jpeojtrs.sca.prf.PrfPackage;
+import mil.jpeojtrs.sca.prf.SimpleRef;
+import mil.jpeojtrs.sca.prf.SimpleSequenceRef;
+import mil.jpeojtrs.sca.prf.StructRef;
+import mil.jpeojtrs.sca.prf.StructSequenceRef;
+import mil.jpeojtrs.sca.prf.Values;
 import mil.jpeojtrs.sca.sad.ExternalProperties;
 import mil.jpeojtrs.sca.sad.ExternalProperty;
 import mil.jpeojtrs.sca.sad.SadComponentInstantiation;
@@ -24,9 +39,11 @@ import mil.jpeojtrs.sca.sad.SoftwareAssembly;
 import mil.jpeojtrs.sca.util.ScaEcoreUtils;
 
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.util.FeatureMap.ValueListIterator;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.nebula.widgets.xviewer.edit.CellEditDescriptor;
@@ -140,8 +157,8 @@ public class PropertiesViewerConverter implements XViewerConverter {
 				}
 			}
 		} else if (ced.getInputField().equals(PropertiesViewerFactory.SAD_VALUE.getId())) {
-			if (selObject instanceof ViewerSimpleProperty) {
-				ViewerSimpleProperty prop = (ViewerSimpleProperty) selObject;
+			if (selObject instanceof ViewerProperty< ? >) {
+				ViewerProperty< ? > prop = (ViewerProperty< ? >) selObject;
 				String value = null;
 				if (c instanceof Combo) {
 					Combo combo = (Combo) c;
@@ -150,21 +167,139 @@ public class PropertiesViewerConverter implements XViewerConverter {
 					Text text = (Text) c;
 					value = text.getText();
 				}
-				
-				if (value == null || PluginUtil.equals(prop.getValue(), value)) {
+
+				if (value == null) {
 					return;
 				}
-				
+
 				SadComponentInstantiation compInst = prop.getComponentInstantiation();
-				Command command;
+				Command command = null;
 				ComponentProperties properties = compInst.getComponentProperties();
 				if (properties != null) {
-					
+					AbstractPropertyRef< ? > ref = getRef(prop, properties);
+					if (ref != null) {
+						if (value.isEmpty()) {
+							if (properties.getProperties().size() > 1) {
+								command = createRemoveRefCommand(ref);
+							} else {
+								command = createRemovePropertiesCommand(properties, compInst);
+							}
+						} else {
+							command = createSetValueCommand(ref, prop, value);
+						}
+					} else {
+						command = addRefCommand(ref, prop, value);
+					}
 				} else {
-					
+					command = createPropertiesCommand(compInst, prop, value);
+				}
+
+				if (command != null) {
+					TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(compInst);
+					domain.getCommandStack().execute(command);
 				}
 			}
 		}
+	}
+
+	private Command createPropertiesCommand(SadComponentInstantiation compInst, ViewerProperty< ? > prop, String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Command createRemovePropertiesCommand(ComponentProperties properties, SadComponentInstantiation compInst) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Command createRemoveRefCommand(AbstractPropertyRef< ? > ref) {
+		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(ref);
+		if (ref instanceof SimpleRef) {
+			if (ref.eContainer() instanceof ComponentProperties) {
+				return RemoveCommand.create(domain, ref.eContainer(), PartitioningPackage.Literals.COMPONENT_PROPERTIES__SIMPLE_REF, ref);
+			} else if (ref.eContainer() instanceof StructRef) {
+				if (((StructRef) ref.eContainer()).getSimpleRef().size() == 1) {
+					return createRemoveRefCommand(((StructRef) ref.eContainer()));
+				}
+				return RemoveCommand.create(domain, ref.eContainer(), PrfPackage.Literals.STRUCT_REF__SIMPLE_REF, ref);
+			}
+		} else if (ref instanceof SimpleSequenceRef) {
+			return RemoveCommand.create(domain, ref.eContainer(), PartitioningPackage.Literals.COMPONENT_PROPERTIES__SIMPLE_SEQUENCE_REF, ref);
+		} else if (ref instanceof StructRef) {
+			if (ref.eContainer() instanceof ComponentProperties) {
+				return RemoveCommand.create(domain, ref.eContainer(), PartitioningPackage.Literals.COMPONENT_PROPERTIES__STRUCT_REF, ref);
+			}
+			// TODO Handle Struct Sequence
+		} else if (ref instanceof StructSequenceRef) {
+			return RemoveCommand.create(domain, ref.eContainer(), PartitioningPackage.Literals.COMPONENT_PROPERTIES__STRUCT_SEQUENCE_REF, ref);
+		}
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Command addRefCommand(AbstractPropertyRef< ? > ref, ViewerProperty< ? > prop, String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Command createSetValueCommand(AbstractPropertyRef< ? > ref, ViewerProperty< ? > prop, String value) {
+		if (prop instanceof ViewerSimpleProperty && ref instanceof SimpleRef) {
+			SimpleRef simpleRef = (SimpleRef) ref;
+			if (PluginUtil.equals(((ViewerSimpleProperty) prop).getValue(), value)) {
+				return null;
+			}
+			ViewerSimpleProperty simpleProp = (ViewerSimpleProperty) prop;
+			if (simpleProp.getDefinition().getType().isValueOfType(value, simpleProp.getDefinition().isComplex())) {
+				TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(ref);
+				return SetCommand.create(domain, simpleRef, PrfPackage.Literals.SIMPLE_REF__VALUE, value);
+			}
+		} else if (prop instanceof ViewerSequenceProperty && ref instanceof SimpleSequenceRef) {
+			SimpleSequenceRef seqRef = (SimpleSequenceRef) ref;
+			ViewerSequenceProperty seqProp = (ViewerSequenceProperty) prop;
+			String[] input = value.split(",");
+			List<String> newValues = new ArrayList<String>(input.length);
+			for (int i = 0; i < input.length; i++) {
+				String newValue = input[i].trim();
+				if (!seqProp.getDefinition().getType().isValueOfType(newValue, seqProp.getDefinition().isComplex())) {
+					// One of the values is bad, abort
+					return null;
+				}
+				newValues.add(newValue);
+			}
+			if (Arrays.equals(newValues.toArray(), seqRef.getValues().getValue().toArray())) {
+				return null;
+			}
+			TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(ref);
+			Values seqValues = PrfFactory.eINSTANCE.createValues();
+			seqValues.getValue().addAll(newValues);
+			return SetCommand.create(domain, seqRef, PrfPackage.Literals.SIMPLE_SEQUENCE__VALUES, seqValues);
+		}
+		return null;
+	}
+
+	private AbstractPropertyRef< ? > getRef(ViewerProperty< ? > prop, ComponentProperties properties) {
+		if (prop.getParent() instanceof ViewerComponent) {
+			for (ValueListIterator<Object> i = properties.getProperties().valueListIterator(); i.hasNext();) {
+				Object obj = i.next();
+				if (obj instanceof AbstractPropertyRef< ? > && PluginUtil.equals(((AbstractPropertyRef< ? >) obj).getRefID(), prop.getID())) {
+					return (AbstractPropertyRef< ? >) obj;
+				}
+			}
+		} else if (prop.getParent() instanceof ViewerProperty< ? >) {
+			AbstractPropertyRef< ? > parentRef = getRef((ViewerProperty< ? >) prop.getParent(), properties);
+			if (prop instanceof ViewerSimpleProperty && parentRef instanceof StructRef) {
+				StructRef structRef = (StructRef) parentRef;
+				ViewerSimpleProperty simpleProp = (ViewerSimpleProperty) prop;
+				for (SimpleRef ref : structRef.getSimpleRef()) {
+					if (PluginUtil.equals(ref.getRefID(), simpleProp.getID())) {
+						return ref;
+					}
+				}
+			}
+			// TODO Handle Struct Sequence Editing
+		}
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
