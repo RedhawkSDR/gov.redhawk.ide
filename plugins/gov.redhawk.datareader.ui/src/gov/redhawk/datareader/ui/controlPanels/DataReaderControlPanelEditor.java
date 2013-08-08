a3d7f0872d0654ff83b6c8b0acd40830ba8af537
Erik Englund <erik.englund@axiosengineering.com>
2013-08-08 07:43:26 -0400

    refs #818 Added the new IDE tools Feature. Organized imports.

--- plugins/gov.redhawk.datareader.ui/src/gov/redhawk/datareader/ui/controlPanels/DataReaderControlPanelEditor.java
+++ plugins/gov.redhawk.datareader.ui/src/gov/redhawk/datareader/ui/controlPanels/DataReaderControlPanelEditor.java
@@ -10,37 +10,12 @@
  *******************************************************************************/
 package gov.redhawk.datareader.ui.controlPanels;
 
-import gov.redhawk.model.sca.IRefreshable;
-import gov.redhawk.model.sca.RefreshDepth;
 import gov.redhawk.model.sca.ScaComponent;
 import gov.redhawk.sca.ui.editors.AbstractScaContentEditor;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
-import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.progress.UIJob;
-
-import CF.Resource;
 
 /**
  * An example showing how to create a control panel.
