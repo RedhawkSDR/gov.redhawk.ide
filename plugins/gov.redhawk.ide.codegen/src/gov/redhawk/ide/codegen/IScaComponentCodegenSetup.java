6d16787261828fd47a58e0d3b7c0b2b2f26fb6e6
Erik Englund <erik.englund@axiosengineering.com>
2014-06-09 12:33:28 -0400

    IDE-714 #time 4h #comment Initial commit to support system checks in wizards.

---
+++ plugins/gov.redhawk.ide.codegen/src/gov/redhawk/ide/codegen/IScaComponentCodegenSetup.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.ide.codegen;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+/**
+ * Allows a code generator to perform system tests during project creation to ensure the
+ * system will support development of the component type in question.
+ * @since 10.0
+ */
+public interface IScaComponentCodegenSetup extends IScaComponentCodegen {
+
+	/**
+	 * Perform various system tests to ensure the system supports development of the given template.
+	 * 
+	 * For example test could include, checking for header files, architecture, cross platform support etc.
+	 * @param templateId The template to check for
+	 * @throws CoreException If the system does not support requested template or other errors occur during testing 
+	 */
+	public void checkSystem(IProgressMonitor monitor, String templateId) throws CoreException;
+}
--- plugins/gov.redhawk.ide.spd.ui/src/gov/redhawk/ide/spd/ui/wizard/NewScaResourceWizard.java
+++ plugins/gov.redhawk.ide.spd.ui/src/gov/redhawk/ide/spd/ui/wizard/NewScaResourceWizard.java
@@ -13,7 +13,9 @@ package gov.redhawk.ide.spd.ui.wizard;
 import gov.redhawk.ide.codegen.CodegenFactory;
 import gov.redhawk.ide.codegen.CodegenUtil;
 import gov.redhawk.ide.codegen.ICodeGeneratorDescriptor;
+import gov.redhawk.ide.codegen.ICodeGeneratorsRegistry;
 import gov.redhawk.ide.codegen.IScaComponentCodegen;
+import gov.redhawk.ide.codegen.IScaComponentCodegenSetup;
 import gov.redhawk.ide.codegen.ITemplateDesc;
 import gov.redhawk.ide.codegen.ImplementationSettings;
 import gov.redhawk.ide.codegen.RedhawkCodegenActivator;
@@ -34,6 +36,7 @@ import gov.redhawk.model.sca.util.ModelUtil;
 import gov.redhawk.sca.util.SubMonitor;
 
 import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -58,13 +61,17 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
@@ -217,8 +224,10 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 						// Store the generator page if it's next, otherwise
 						// we'll be inserting one
 
-						// If the wizard has an old codeGen page we need to snatch up then the wizards size will be greater or equal than codeGen index + 1.
-						// If the wizard has an old codeGen page we need to snatch up then the old codeGen page will be of type ICodegenWizardPage
+						// If the wizard has an old codeGen page we need to snatch up then the wizards size will be
+						// greater or equal than codeGen index + 1.
+						// If the wizard has an old codeGen page we need to snatch up then the old codeGen page will be
+						// of type ICodegenWizardPage
 						if (codegenIndex + 1 <= this.wizPages.size() && (this.wizPages.get(codegenIndex) instanceof ICodegenWizardPage)) {
 							ICodegenWizardPage[] oldCodeGenPages = RedhawkCodegenUiActivator.getCodeGeneratorsRegistry().findPageByGeneratorId(previousImplId);
 							numOfOldGenPages = oldCodeGenPages.length;
@@ -580,6 +589,7 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 	 */
 	@Override
 	public boolean performFinish() {
+
 		this.updateEntryPoints();
 		final boolean isCreateNewResource = this.resourcePropertiesPage.isCreateNewResource();
 		final IWorkingSet[] workingSets = this.resourcePropertiesPage.getSelectedWorkingSets();
@@ -599,7 +609,41 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 			@Override
 			protected void execute(final IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
 				try {
-					final SubMonitor progress = SubMonitor.convert(monitor, "Creating project...", 4 + getImplList().size()); // SUPPRESS CHECKSTYLE MagicNumber
+					final SubMonitor progress = SubMonitor.convert(monitor, "Creating project...", 5 + getImplList().size()); // SUPPRESS
+																																// CHECKSTYLE
+																																// MagicNumber
+					// Create the implementation
+					final ImplementationWizardPage page = (ImplementationWizardPage) getWizPages().get(1);
+					final Implementation pageImpl = page.getImplementation();
+					final ImplementationSettings settings = page.getImplSettings();
+
+					SubMonitor checkMonitor = progress.newChild(1);
+					checkMonitor.beginTask("Checking System dependencies...", getImplList().size());
+					for (final ImplementationAndSettings implAndSettings : getImplList()) {
+						try {
+							checkSettings(checkMonitor.newChild(1), implAndSettings);
+						} catch (final CoreException e) {
+							final boolean[] retVal = { false };
+							PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+								@Override
+								public void run() {
+									String title = "System Check Failed";
+									String msg = "Errors occured while checking system dependencies for {0}:{1}.\n\nDo you wish to continue?\n\n{2}";
+									String formatedMsg = MessageFormat.format(msg, implAndSettings.getImplementationSettings().getGeneratorId(),
+										implAndSettings.getImplementationSettings().getTemplate(), e.getMessage());
+									MessageDialog dialog = new MessageDialog(Display.getCurrent().getActiveShell(), title, null, formatedMsg,
+										MessageDialog.WARNING, new String[] { IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL }, 1);
+									dialog.open();
+									retVal[0] = dialog.getReturnCode() == 0;
+								}
+
+							});
+							if (!retVal[0]) {
+								throw new OperationCanceledException();
+							}
+						}
+					}
 
 					// Create an empty project
 					final IProject project = createEmptyProject(projectName, locationURI, progress.newChild(1));
@@ -614,17 +658,14 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 							// Create the SCA XML files
 							setOpenEditorOn(createComponentFiles(project, projectName, getSoftPkg().getId(), null, progress.newChild(1)));
 
-							// Create the implementation
-							final ImplementationWizardPage page = (ImplementationWizardPage) getWizPages().get(1);
-							final Implementation pageImpl = page.getImplementation();
-							final ImplementationSettings settings = page.getImplSettings();
 							ProjectCreator.addImplementation(project, projectName, pageImpl, settings, progress.newChild(1));
 						} else {
 							setOpenEditorOn(ProjectCreator.importFiles(project, existingResourceLocation, getImplList(), getImportedSettingsMap(),
 								progress.newChild(2), getSoftPkg().getId()));
 						}
 
-						String spdFileName = project.getName() + SpdPackage.FILE_EXTENSION; // SUPPRESS CHECKSTYLE AvoidInLine
+						String spdFileName = project.getName() + SpdPackage.FILE_EXTENSION; // SUPPRESS CHECKSTYLE
+																							// AvoidInLine
 						final IFile spdFile = project.getFile(spdFileName);
 
 						// Allows for subclasses to modify the project
@@ -674,6 +715,12 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 					}
 				} catch (final CoreException e) {
 					throw e;
+				} catch (InterruptedException e) {
+					throw e;
+				} catch (OperationCanceledException e) {
+					throw e;
+				} catch (InvocationTargetException e) {
+					throw e;
 				} catch (final Exception e) { // SUPPRESS CHECKSTYLE Logged Catch all exception
 					throw new CoreException(new Status(IStatus.ERROR, ComponentUiPlugin.PLUGIN_ID, "Error creating project", e));
 				} finally {
@@ -681,6 +728,17 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 					if (monitor != null) {
 						monitor.done();
 					}
+				}
+			}
+
+			private void checkSettings(IProgressMonitor monitor, final ImplementationAndSettings implAndSettings) throws CoreException {
+				ICodeGeneratorsRegistry registry = RedhawkCodegenActivator.getCodeGeneratorsRegistry();
+				ICodeGeneratorDescriptor desc = registry.findCodegen(implAndSettings.getImplementationSettings().getGeneratorId());
+				IScaComponentCodegen gen;
+				gen = desc.getGenerator();
+				if (gen instanceof IScaComponentCodegenSetup) {
+					IScaComponentCodegenSetup setup = (IScaComponentCodegenSetup) gen;
+					setup.checkSystem(monitor, implAndSettings.getImplementationSettings().getTemplate());
 				}
 			}
 
@@ -708,7 +766,7 @@ public abstract class NewScaResourceWizard extends Wizard implements INewWizard,
 			}
 			return false;
 		} catch (final InterruptedException e1) {
-			return true;
+			return false;
 		}
 
 	}
