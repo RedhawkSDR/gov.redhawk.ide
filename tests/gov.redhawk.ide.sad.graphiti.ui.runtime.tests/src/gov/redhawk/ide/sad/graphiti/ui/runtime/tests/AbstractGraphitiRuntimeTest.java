a05bb0484322402241cb6066002f84e2cf5a7470
Devin Nelson <devin.nelson@axiosengineering.com>
2014-11-06 14:38:21 -0500

    updated superclass for runtime swtbot tests to clear out sandbox at the end of each test.

--- tests/gov.redhawk.ide.sad.graphiti.ui.runtime.tests/src/gov/redhawk/ide/sad/graphiti/ui/runtime/tests/AbstractGraphitiRuntimeTest.java
+++ tests/gov.redhawk.ide.sad.graphiti.ui.runtime.tests/src/gov/redhawk/ide/sad/graphiti/ui/runtime/tests/AbstractGraphitiRuntimeTest.java
@@ -12,7 +12,12 @@ package gov.redhawk.ide.sad.graphiti.ui.runtime.tests;
 
 import gov.redhawk.ide.swtbot.UIRuntimeTest;
 
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.After;
 import org.junit.Before;
 
 /**
@@ -22,11 +27,21 @@ public abstract class AbstractGraphitiRuntimeTest extends UIRuntimeTest {
 
 	protected SWTGefBot gefBot; // SUPPRESS CHECKSTYLE INLINE
 
-
 	@Before
 	public void beforeTest() throws Exception {
 		gefBot = new SWTGefBot();
 		super.before();
+	}
+
+	@After
+	public void afterTest() {
+		// Clear out the Sandbox
+		SWTBotView scaExplorerView = gefBot.viewByTitle("SCA Explorer");
+		SWTBotTree scaTree = scaExplorerView.bot().tree();
+		SWTBotTreeItem chalkboard = scaTree.expandNode("Sandbox", "Chalkboard");
+		chalkboard.select();
+		SWTBotMenu release = chalkboard.contextMenu("Release");
+		release.click();
 	}
 
 }
