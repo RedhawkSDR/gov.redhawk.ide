ed53e412a78eadda3bfc68df12c1c3a59ae31d25
Dave Hickernell <david.hickernell@axiosengineering.com>
2014-11-19 16:03:50 -0500

    IDE-692 Broke palette behavior class out for clarity

--- plugins/gov.redhawk.ide.sad.graphiti.ui/src/gov/redhawk/ide/sad/graphiti/ui/diagram/RHGraphitiDiagramEditor.java
+++ plugins/gov.redhawk.ide.sad.graphiti.ui/src/gov/redhawk/ide/sad/graphiti/ui/diagram/RHGraphitiDiagramEditor.java
@@ -10,38 +10,14 @@
  *******************************************************************************/
 package gov.redhawk.ide.sad.graphiti.ui.diagram;
 
-import gov.redhawk.core.resourcefactory.ComponentDesc;
-import gov.redhawk.core.resourcefactory.IResourceFactoryRegistry;
-import gov.redhawk.core.resourcefactory.ResourceDesc;
-import gov.redhawk.core.resourcefactory.ResourceFactoryPlugin;
-import gov.redhawk.ide.sad.graphiti.ui.diagram.providers.RHToolBehaviorProvider;
-import gov.redhawk.ide.sad.graphiti.ui.palette.RHGraphitiPaletteFilter;
-import gov.redhawk.ide.sad.graphiti.ui.palette.RHGraphitiPaletteFilterEditPart;
-import gov.redhawk.ide.sad.graphiti.ui.palette.RHGraphitiPaletteRoot;
+import gov.redhawk.ide.sad.graphiti.ui.palette.RHGraphitiPaletteBehavior;
+import gov.redhawk.ide.sad.ui.providers.SpdToolEntry;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import mil.jpeojtrs.sca.scd.ComponentType;
 
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer.Delegate;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.internal.ui.palette.editparts.GroupEditPart;
-import org.eclipse.gef.palette.PaletteDrawer;
-import org.eclipse.gef.palette.PaletteEntry;
-import org.eclipse.gef.palette.PaletteGroup;
-import org.eclipse.gef.palette.PaletteRoot;
-import org.eclipse.gef.palette.PaletteSeparator;
-import org.eclipse.gef.palette.PaletteStack;
-import org.eclipse.gef.ui.palette.PaletteEditPartFactory;
-import org.eclipse.gef.ui.palette.PaletteViewer;
-import org.eclipse.gef.ui.palette.PaletteViewerProvider;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.IUpdateFeature;
@@ -52,13 +28,11 @@ import org.eclipse.graphiti.ui.editor.DefaultUpdateBehavior;
 import org.eclipse.graphiti.ui.editor.DiagramBehavior;
 import org.eclipse.graphiti.ui.editor.DiagramEditor;
 import org.eclipse.jface.util.TransferDropTargetListener;
-import org.eclipse.swt.widgets.Composite;
 
 @SuppressWarnings("restriction")
 public class RHGraphitiDiagramEditor extends DiagramEditor {
 
 	private EditingDomain editingDomain;
-	private RHGraphitiPaletteFilter paletteFilter;
 
 	public RHGraphitiDiagramEditor(EditingDomain editingDomain) {
 		this.editingDomain = editingDomain;
@@ -102,81 +76,7 @@ public class RHGraphitiDiagramEditor extends DiagramEditor {
 
 				};
 			}
-
-			final class RHGraphitiPaletteBehavior extends DefaultPaletteBehavior {
-
-				public RHGraphitiPaletteBehavior(DiagramBehavior diagramBehavior) {
-					super(diagramBehavior);
-				}
-
-				@Override
-				protected PaletteRoot createPaletteRoot() {
-					return new RHGraphitiPaletteRoot(diagramBehavior.getDiagramTypeProvider());
-				}
-
-				@Override
-				public void refreshPalette() {
-					RHGraphitiPaletteRoot root = (RHGraphitiPaletteRoot) this.getPaletteRoot();
-					root.updatePaletteEntries();
-				}
-
-				@Override
-				protected PaletteViewerProvider createPaletteViewerProvider() {
-					return new PaletteViewerProvider(getEditDomain()) {
-
-						public PaletteViewer createPaletteViewer(Composite parent) {
-							final PaletteViewer viewer = new PaletteViewer();
-							viewer.setEditPartFactory(new PaletteEditPartFactory() {
-
-								/*
-								 * @see org.eclipse.gef.ui.palette.PaletteEditPartFactory#createToolbarEditPart(org.eclipse.gef.EditPart, java.lang.Object)
-								 */
-								@SuppressWarnings("unchecked")
-								@Override
-								protected EditPart createGroupEditPart(EditPart parentEditPart, final Object model) {
-									GroupEditPart retVal = new GroupEditPart((PaletteGroup) model) {
-
-										/*
-										 * @see org.eclipse.gef.editparts.AbstractEditPart#createChild(java.lang.Object)
-										 */
-										@Override
-										protected EditPart createChild(Object model) {
-											if ("Text".equals(model)) {
-												RHGraphitiPaletteFilterEditPart label = new RHGraphitiPaletteFilterEditPart(viewer);
-												setPaletteFilter(label.getPaletteFilter());
-												getPaletteFilter().setPaletteBehavior(RHGraphitiPaletteBehavior.this);
-												((RHToolBehaviorProvider) RHGraphitiDiagramEditor.this.getDiagramTypeProvider().getCurrentToolBehaviorProvider()).setFilter(getPaletteFilter());
-												return label;
-											}
-											return super.createChild(model);
-										}
-
-										/*
-										 * @see org.eclipse.gef.ui.palette.editparts.PaletteEditPart#getModelChildren()
-										 */
-										@Override
-										public List<Object> getModelChildren() {
-											List<Object> retVal = super.getModelChildren();
-											retVal.add(0, "Text");
-											retVal.add(1, new PaletteSeparator());
-											return retVal;
-										}
-									};
-
-									return retVal;
-								}
-							});
-
-							viewer.createControl(parent);
-							configurePaletteViewer(viewer);
-							hookPaletteViewer(viewer);
-							return viewer;
-						}
-
-					};
-				}
-			}
-
+			
 			@Override
 			protected DefaultPaletteBehavior createPaletteBehaviour() {
 				final DefaultPaletteBehavior paletteBehavior = new RHGraphitiPaletteBehavior(this);
@@ -191,96 +91,6 @@ public class RHGraphitiDiagramEditor extends DiagramEditor {
 			}
 
 		};
-	}
-
-	protected List<PaletteEntry> getFilteredContents(PaletteRoot root) {
-		Map<String, List<PaletteEntry>> containerMap = new HashMap<String, List<PaletteEntry>>();
-
-		IResourceFactoryRegistry registry = ResourceFactoryPlugin.getDefault().getResourceFactoryRegistry();
-		for (ResourceDesc desc : registry.getResourceDescriptors()) {
-			if (!shouldAdd(desc)) {
-				continue;
-			}
-			String category = desc.getCategory();
-			List<PaletteEntry> containerList = containerMap.get(category);
-			if (containerList == null) {
-				String label = category;
-				if (label == null) {
-					label = "Other";
-				}
-				containerList = new ArrayList<PaletteEntry>();
-				containerMap.put(category, containerList);
-			}
-			if (desc instanceof ComponentDesc) {
-				PaletteEntry newEntry = null;
-				ComponentDesc compDesc = (ComponentDesc) desc;
-				List<PaletteEntry> entries = createPaletteEntries(compDesc);
-				sort(entries);
-				if (entries.size() > 1) {
-					PaletteStack stack = new PaletteStack(entries.get(0).getLabel(), entries.get(0).getDescription(), entries.get(0).getLargeIcon());
-					stack.setChildren(entries);
-					newEntry = stack;
-				} else if (!entries.isEmpty()) {
-					newEntry = entries.get(0);
-				}
-				if (newEntry != null) {
-					containerList.add(newEntry);
-				}
-			}
-		}
-
-		final List<PaletteEntry> retVal = new ArrayList<PaletteEntry>();
-		Object tools = root.getChildren().get(0);
-		if (tools instanceof PaletteGroup) {
-			retVal.add((PaletteGroup) tools);
-		}
-		for (Map.Entry<String, List<PaletteEntry>> entry : containerMap.entrySet()) {
-			PaletteDrawer container = new PaletteDrawer(entry.getKey());
-			sort(entry.getValue());
-			container.addAll(entry.getValue());
-			retVal.add(container);
-		}
-		sort(retVal);
-
-		return retVal;
-	}
-
-	private boolean shouldAdd(ResourceDesc desc) {
-		if (desc instanceof ComponentDesc) {
-			ComponentDesc compDesc = (ComponentDesc) desc;
-			String type = compDesc.getComponentType();
-			return ComponentType.RESOURCE.getLiteral().equalsIgnoreCase(type);
-		} else {
-			return false;
-		}
-	}
-
-	private void sort(List<PaletteEntry> entries) {
-		Collections.sort(entries, new Comparator<PaletteEntry>() {
-
-			@Override
-			public int compare(final PaletteEntry o1, final PaletteEntry o2) {
-				final String str1 = o1.getLabel();
-				final String str2 = o2.getLabel();
-				if (str1 == null) {
-					if (str2 == null) {
-						return 0;
-					} else {
-						return 1;
-					}
-				} else if (str2 == null) {
-					return -1;
-				} else {
-					return str1.compareToIgnoreCase(str2);
-				}
-			}
-
-		});
-	}
-
-	private List<PaletteEntry> createPaletteEntries(ComponentDesc desc) {
-		List<PaletteEntry> retVal = new ArrayList<PaletteEntry>(desc.getImplementationIds().size());
-		return retVal;
 	}
 
 	// This is a major hack that lets the diagram update when changes are made in the overview tab
@@ -299,11 +109,4 @@ public class RHGraphitiDiagramEditor extends DiagramEditor {
 		}
 	}
 
-	public RHGraphitiPaletteFilter getPaletteFilter() {
-		return paletteFilter;
-	}
-
-	public void setPaletteFilter(RHGraphitiPaletteFilter paletteFilter) {
-		this.paletteFilter = paletteFilter;
-	}
 }
