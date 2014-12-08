81e2363d779a978f59fbfa3279c9ba5f7f10fd76
Devin Nelson <devin.nelson@axiosengineering.com>
2014-12-08 11:41:39 -0500

    added device create feature, updated device pattern

--- plugins/gov.redhawk.ide.graphiti.dcd.ui/src/gov/redhawk/ide/graphiti/dcd/ui/diagram/features/create/DeviceCreateFeature.java
+++ plugins/gov.redhawk.ide.graphiti.dcd.ui/src/gov/redhawk/ide/graphiti/dcd/ui/diagram/features/create/DeviceCreateFeature.java
@@ -11,25 +11,41 @@
  */
 package gov.redhawk.ide.graphiti.dcd.ui.diagram.features.create;
 
+import gov.redhawk.ide.graphiti.ui.diagram.util.DUtil;
+import gov.redhawk.sca.util.PluginUtil;
+import mil.jpeojtrs.sca.dcd.DcdComponentInstantiation;
+import mil.jpeojtrs.sca.dcd.DcdComponentPlacement;
+import mil.jpeojtrs.sca.dcd.DcdFactory;
+import mil.jpeojtrs.sca.dcd.DeviceConfiguration;
+import mil.jpeojtrs.sca.partitioning.ComponentFile;
+import mil.jpeojtrs.sca.partitioning.ComponentFileRef;
+import mil.jpeojtrs.sca.partitioning.ComponentFiles;
+import mil.jpeojtrs.sca.partitioning.PartitioningFactory;
+import mil.jpeojtrs.sca.sad.SadFactory;
 import mil.jpeojtrs.sca.spd.SoftPkg;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalCommandStack;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.ICreateContext;
 import org.eclipse.graphiti.features.impl.AbstractCreateFeature;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 
 public class DeviceCreateFeature extends AbstractCreateFeature {
 
-	// TODO: Device creation feature stub.  Still needs to be imlpemented
-	
+	// TODO: Device creation feature stub. Still needs to be implemented
+
 	private SoftPkg spd = null;
 	private String implId = null;
-	
+
 	@Override
 	public String getDescription() {
 		// Provides the context menu Undo/Redo description
 		return "Add Device to Diagram";
 	}
-	
+
 	public DeviceCreateFeature(IFeatureProvider fp, String name, String description) {
 		super(fp, name, description);
 	}
@@ -40,14 +56,112 @@ public class DeviceCreateFeature extends AbstractCreateFeature {
 		this.implId = implId;
 	}
 
+	// TODO: Allow host collocation? Does that make sense for devices?
 	@Override
 	public boolean canCreate(ICreateContext context) {
+		if (context.getTargetContainer() instanceof Diagram) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public Object[] create(ICreateContext context) {
-		return null;
+
+		if (spd == null) {
+			// TODO: return some kind of error
+			return null;
+		}
+
+		// TODO: ComponentCreateFeature uses Overrides? What's that about?
+
+		// editing domain for our transaction
+		TransactionalEditingDomain editingDomain = getFeatureProvider().getDiagramTypeProvider().getDiagramBehavior().getEditingDomain();
+
+		// Get dcd from diagram
+		final DeviceConfiguration dcd = DUtil.getDiagramDCD(getDiagram());
+
+		// Add to dcd partitioning
+		final EList<DcdComponentPlacement> componentPlacementList = dcd.getPartitioning().getComponentPlacement();
+
+		// container for new component instantiation, necessary for reference after command execution
+		final DcdComponentInstantiation[] componentInstantiations = new DcdComponentInstantiation[1];
+
+		// Create Device related objects in DCD model
+		TransactionalCommandStack stack = (TransactionalCommandStack) editingDomain.getCommandStack();
+		stack.execute(new RecordingCommand(editingDomain) {
+			@Override
+			protected void doExecute() {
+				// add component file
+				ComponentFile componentFile = createComponentFile(dcd, spd);
+
+				// create component placement and add to list
+				final DcdComponentPlacement componentPlacement = DcdFactory.eINSTANCE.createDcdComponentPlacement();
+				componentPlacementList.add(componentPlacement);
+
+				// create component file ref
+				final ComponentFileRef ref = PartitioningFactory.eINSTANCE.createComponentFileRef();
+				ref.setFile(componentFile);
+				componentPlacement.setComponentFileRef(ref);
+
+				// component instantiation
+				componentInstantiations[0] = createComponentInstantiation(dcd, componentPlacement, spd);
+			}
+		});
+
+		// Call add feature
+		addGraphicalRepresentation(context, componentInstantiations[0]);
+
+		return new Object[] { componentInstantiations[0] };
+	}
+
+	// adds corresponding component file to sad if not already present
+	private ComponentFile createComponentFile(final DeviceConfiguration dcd, final SoftPkg spd) {
+
+		// See if we have to add a new component file
+		ComponentFile file = null;
+		// set component files is not already set
+		ComponentFiles cFiles = dcd.getComponentFiles();
+		if (cFiles == null) {
+			cFiles = PartitioningFactory.eINSTANCE.createComponentFiles();
+			dcd.setComponentFiles(cFiles);
+		}
+		// search for existing compatible component file for spd
+		for (final ComponentFile f : cFiles.getComponentFile()) {
+			if (f == null) {
+				continue;
+			}
+			final SoftPkg fSpd = f.getSoftPkg();
+			if (fSpd != null && PluginUtil.equals(spd.getId(), fSpd.getId())) {
+				file = f;
+				break;
+			}
+		}
+		// add new component file if not found above
+		if (file == null) {
+			file = SadFactory.eINSTANCE.createComponentFile();
+			cFiles.getComponentFile().add(file);
+			file.setSoftPkg(spd);
+		}
+
+		return file;
+	}
+
+	// create ComponentInstantiation
+	private DcdComponentInstantiation createComponentInstantiation(final DeviceConfiguration dcd, DcdComponentPlacement componentPlacement, SoftPkg spd) {
+		DcdComponentInstantiation dcdComponentInstantiation = DcdFactory.eINSTANCE.createDcdComponentInstantiation();
+
+		String deviceName = DeviceConfiguration.Util.createDeviceUsageName(dcd, spd.getName());
+		String id = DeviceConfiguration.Util.createDeviceIdentifier(dcd, deviceName);
+
+		dcdComponentInstantiation.setUsageName(deviceName);
+		dcdComponentInstantiation.setId(id);
+		dcdComponentInstantiation.setImplID(implId);
+
+		// add to placement
+		componentPlacement.getComponentInstantiation().add(dcdComponentInstantiation);
+
+		return dcdComponentInstantiation;
 	}
 
 }
--- plugins/gov.redhawk.ide.graphiti.dcd.ui/src/gov/redhawk/ide/graphiti/dcd/ui/diagram/patterns/DevicePattern.java
+++ plugins/gov.redhawk.ide.graphiti.dcd.ui/src/gov/redhawk/ide/graphiti/dcd/ui/diagram/patterns/DevicePattern.java
@@ -11,30 +11,290 @@
  */
 package gov.redhawk.ide.graphiti.dcd.ui.diagram.patterns;
 
+import gov.redhawk.ide.graphiti.dcd.ext.DeviceShape;
+import gov.redhawk.ide.graphiti.dcd.ext.RHDeviceGxFactory;
+import gov.redhawk.ide.graphiti.dcd.ui.diagram.providers.DeviceImageProvider;
+import gov.redhawk.ide.graphiti.ext.impl.RHContainerShapeImpl;
+import gov.redhawk.ide.graphiti.ui.diagram.patterns.AbstractContainerPattern;
+import gov.redhawk.ide.graphiti.ui.diagram.util.DUtil;
+import gov.redhawk.ide.graphiti.ui.diagram.util.StyleUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mil.jpeojtrs.sca.dcd.DcdComponentInstantiation;
+import mil.jpeojtrs.sca.dcd.DeviceConfiguration;
+import mil.jpeojtrs.sca.partitioning.ComponentSupportedInterfaceStub;
+import mil.jpeojtrs.sca.partitioning.ProvidesPortStub;
+import mil.jpeojtrs.sca.partitioning.UsesPortStub;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalCommandStack;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.graphiti.features.IReason;
+import org.eclipse.graphiti.features.context.IAddContext;
+import org.eclipse.graphiti.features.context.IDirectEditingContext;
+import org.eclipse.graphiti.features.context.ILayoutContext;
+import org.eclipse.graphiti.features.context.IMoveShapeContext;
+import org.eclipse.graphiti.features.context.IResizeShapeContext;
+import org.eclipse.graphiti.features.context.IUpdateContext;
+import org.eclipse.graphiti.mm.Property;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Text;
+import org.eclipse.graphiti.mm.algorithms.styles.Style;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
-import org.eclipse.graphiti.pattern.AbstractPattern;
 import org.eclipse.graphiti.pattern.IPattern;
+import org.eclipse.graphiti.services.Graphiti;
 
 // TODO: This should extend AbstractContainerPattern, which is currently located in the graphiti.sad plugin.  
 // Waiting for that class to be refactored out before pointing it here
-public class DevicePattern extends AbstractPattern implements IPattern {
+public class DevicePattern extends AbstractContainerPattern implements IPattern {
+
+	public DevicePattern() {
+		super(null);
+	}
 
 	@Override
+	public String getCreateName() {
+		return "Device";
+	}
+
+	// THE FOLLOWING THREE METHODS DETERMINE IF PATTERN IS APPLICABLE TO OBJECT
+	@Override
 	public boolean isMainBusinessObjectApplicable(Object mainBusinessObject) {
-		// TODO Auto-generated method stub
-		return false;
+		return mainBusinessObject instanceof DcdComponentInstantiation;
 	}
 
 	@Override
 	protected boolean isPatternControlled(PictogramElement pictogramElement) {
-		// TODO Auto-generated method stub
-		return false;
+		Object domainObject = getBusinessObjectForPictogramElement(pictogramElement);
+		return isMainBusinessObjectApplicable(domainObject);
 	}
 
 	@Override
 	protected boolean isPatternRoot(PictogramElement pictogramElement) {
-		// TODO Auto-generated method stub
+		Object domainObject = getBusinessObjectForPictogramElement(pictogramElement);
+		return isMainBusinessObjectApplicable(domainObject);
+	}
+
+	@Override
+	public boolean canAdd(IAddContext context) {
+		if (context.getNewObject() instanceof DcdComponentInstantiation) {
+			if (context.getTargetContainer() instanceof Diagram) {
+				return true;
+			}
+		}
 		return false;
 	}
 
+	/**
+	 * Adds a Device to the diagram. Immediately calls resize at the end to keep sizing and location in one place.
+	 */
+	@Override
+	public PictogramElement add(IAddContext context) {
+
+		// creates shape
+		DeviceShape deviceShape = RHDeviceGxFactory.eINSTANCE.createDeviceShape();
+		deviceShape.init(context, this);
+
+		// set shape location to user's selection
+		Graphiti.getGaLayoutService().setLocation(deviceShape.getGraphicsAlgorithm(), context.getX(), context.getY());
+
+		StyleUtil.createStyleForUsesPort(getDiagram());
+		StyleUtil.createStyleForProvidesPort(getDiagram());
+
+		// layout
+		deviceShape.layout();
+
+		// Check for any needed location adjustments, avoids accidentally stacking shapes
+		// TODO: Implement this method, see ComponentPattern
+		// adjustDeviceLocation(deviceShape);
+
+		return deviceShape;
+	}
+
+	// TODO add canRemove, see Component Pattern
+	// TODO add canDelete, see Component Pattern
+	// TODO add delete, see Component Pattern
+
+	@Override
+	public boolean canResizeShape(IResizeShapeContext context) {
+		return true;
+	}
+
+	@Override
+	public boolean canLayout(ILayoutContext context) {
+		ContainerShape containerShape = (ContainerShape) context.getPictogramElement();
+		Object obj = DUtil.getBusinessObject(containerShape);
+		if (obj instanceof DeviceConfiguration) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Layout children of component
+	 */
+	@Override
+	public boolean layout(ILayoutContext context) {
+		// something is always changing.
+		((DeviceShape) context.getPictogramElement()).layout();
+		return true;
+	}
+
+	public boolean canMoveShape(IMoveShapeContext context) {
+		DcdComponentInstantiation dcdComponentInstantiation = (DcdComponentInstantiation) DUtil.getBusinessObject(context.getPictogramElement());
+		if (dcdComponentInstantiation == null) {
+			return false;
+		}
+
+		// TODO: Another place that needs to be changed if supporting host collocation
+		if (context.getTargetContainer() instanceof Diagram) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public IReason updateNeeded(IUpdateContext context) {
+		return ((DeviceShape) context.getPictogramElement()).updateNeeded(context, this);
+	}
+
+	@Override
+	public boolean canDirectEdit(IDirectEditingContext context) {
+		PictogramElement pe = context.getPictogramElement();
+		DeviceShape deviceShape = (DeviceShape) DUtil.findContainerShapeParentWithProperty(pe, RHContainerShapeImpl.SHAPE_OUTER_CONTAINER);
+		Object obj = getBusinessObjectForPictogramElement(deviceShape);
+		GraphicsAlgorithm ga = context.getGraphicsAlgorithm();
+
+		// allow if we've selected the inner Text for the component
+		if (obj instanceof DcdComponentInstantiation && ga instanceof Text) {
+			Text text = (Text) ga;
+			for (Property prop : text.getProperties()) {
+				if (prop.getValue().equals(RHContainerShapeImpl.GA_INNER_ROUNDED_RECTANGLE_TEXT)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int getEditingType() {
+		return TYPE_TEXT;
+	}
+
+	@Override
+	public String getInitialValue(IDirectEditingContext context) {
+		PictogramElement pe = context.getPictogramElement();
+		DeviceShape deviceShape = (DeviceShape) DUtil.findContainerShapeParentWithProperty(pe, RHContainerShapeImpl.SHAPE_OUTER_CONTAINER);
+		DcdComponentInstantiation ci = (DcdComponentInstantiation) getBusinessObjectForPictogramElement(deviceShape);
+		return ci.getUsageName();
+	}
+
+	@Override
+	public void setValue(final String value, IDirectEditingContext context) {
+		PictogramElement pe = context.getPictogramElement();
+		DeviceShape deviceShape = (DeviceShape) DUtil.findContainerShapeParentWithProperty(pe, RHContainerShapeImpl.SHAPE_OUTER_CONTAINER);
+		final DcdComponentInstantiation ci = (DcdComponentInstantiation) getBusinessObjectForPictogramElement(deviceShape);
+
+		// editing domain for our transaction
+		TransactionalEditingDomain editingDomain = getFeatureProvider().getDiagramTypeProvider().getDiagramBehavior().getEditingDomain();
+
+		// Perform business object manipulation in a Command
+		TransactionalCommandStack stack = (TransactionalCommandStack) editingDomain.getCommandStack();
+		stack.execute(new RecordingCommand(editingDomain) {
+			@Override
+			protected void doExecute() {
+				// set usage name
+				ci.setUsageName(value);
+			}
+		});
+
+		// perform update, redraw
+		updatePictogramElement(deviceShape);
+	}
+
+	@Override
+	public String getOuterTitle(EObject obj) {
+		if (obj instanceof DcdComponentInstantiation) {
+			try {
+				return ((DcdComponentInstantiation) obj).getPlacement().getComponentFileRef().getFile().getSoftPkg().getName();
+			} catch (NullPointerException e) {
+				return "< Component Bad Reference >";
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String getInnerTitle(EObject obj) {
+		if (obj instanceof DcdComponentInstantiation) {
+			return ((DcdComponentInstantiation) obj).getUsageName();
+		}
+		return null;
+	}
+
+	@Override
+	public EList<UsesPortStub> getUses(EObject obj) {
+		if (obj instanceof DcdComponentInstantiation) {
+			return ((DcdComponentInstantiation) obj).getUses();
+		}
+		return null;
+	}
+
+	@Override
+	public EList<ProvidesPortStub> getProvides(EObject obj) {
+		if (obj instanceof DcdComponentInstantiation) {
+			return ((DcdComponentInstantiation) obj).getProvides();
+		}
+		return null;
+	}
+
+	@Override
+	public ComponentSupportedInterfaceStub getInterface(EObject obj) {
+		if (obj instanceof DcdComponentInstantiation) {
+			return ((DcdComponentInstantiation) obj).getInterfaceStub();
+		}
+		return null;
+	}
+
+	@Override
+	public String getOuterImageId() {
+		return DeviceImageProvider.IMG_COMPONENT_PLACEMENT;
+	}
+
+	@Override
+	public String getInnerImageId() {
+		return DeviceImageProvider.IMG_COMPONENT_INSTANCE;
+	}
+
+	@Override
+	public Style createStyleForOuter() {
+		return StyleUtil.createStyleForComponentOuter(getDiagram());
+	}
+
+	@Override
+	public Style createStyleForInner() {
+		return StyleUtil.createStyleForComponentInner(getDiagram());
+	}
+
+	/**
+	 * Returns device, dcd, ports. Order does matter.
+	 */
+	public List<EObject> getBusinessObjectsToLink(EObject componentInstantiation) {
+		// get dcd from diagram, we need to link it to all shapes so the diagram will update when changes occur
+		List<EObject> businessObjectsToLink = new ArrayList<EObject>();
+		DeviceConfiguration dcd = DUtil.getDiagramDCD(getDiagram());
+		// ORDER MATTERS, CI must be first
+		businessObjectsToLink.add(componentInstantiation);
+		businessObjectsToLink.add(dcd);
+
+		return businessObjectsToLink;
+	}
 }
