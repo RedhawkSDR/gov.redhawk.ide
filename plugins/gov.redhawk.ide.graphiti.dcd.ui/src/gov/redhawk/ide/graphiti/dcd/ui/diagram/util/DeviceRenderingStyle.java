55f714d8e612016bdb866ff80584db3f52edded8
Devin Nelson <devin.nelson@axiosengineering.com>
2014-12-31 13:24:44 -0500

    NodeDiagram Util classes

---
+++ plugins/gov.redhawk.ide.graphiti.dcd.ui/src/gov/redhawk/ide/graphiti/dcd/ui/diagram/util/DeviceRenderingStyle.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.ide.graphiti.dcd.ui.diagram.util;
+
+import org.eclipse.graphiti.util.IPredefinedRenderingStyle;
+
+public interface DeviceRenderingStyle extends IPredefinedRenderingStyle {
+	public static final String GREEN_WHITE_ID = "green_white";
+}
