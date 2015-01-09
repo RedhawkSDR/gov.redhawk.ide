95f09e5bab5907530de83537d694f188d4855d05
Devin Nelson <devin.nelson@axiosengineering.com>
2015-01-09 10:46:14 -0500

    updated API version

--- plugins/gov.redhawk.ide.dcd.ui/src/gov/redhawk/ide/dcd/ui/DcdUiActivator.java
+++ plugins/gov.redhawk.ide.dcd.ui/src/gov/redhawk/ide/dcd/ui/DcdUiActivator.java
@@ -21,6 +21,7 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * @since 1.2
  */
 public class DcdUiActivator extends AbstractUIPlugin {
 
