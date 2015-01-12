b9c09e7f5fe6f24267c42494b6a37b856daa1049
Neon Ngo <ntn@techma.com>
2015-01-12 15:33:18 -0500

    IDE-1001 hide grid units on runtime (in Domain) Waveform diagram

--- plugins/gov.redhawk.ide.graphiti.sad.ui/src-editor/gov/redhawk/ide/graphiti/sad/internal/ui/editor/GraphitiWaveformExplorerEditor.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src-editor/gov/redhawk/ide/graphiti/sad/internal/ui/editor/GraphitiWaveformExplorerEditor.java
@@ -11,17 +11,31 @@
 package gov.redhawk.ide.graphiti.sad.internal.ui.editor;
 
 import gov.redhawk.ide.graphiti.ui.diagram.util.DUtil;
+import gov.redhawk.model.sca.commands.ScaModelCommand;
 
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 
 public class GraphitiWaveformExplorerEditor extends GraphitiWaveformSandboxEditor {
 
 	public GraphitiWaveformExplorerEditor() {
 	}
-	
+
 	@Override
 	public String getDiagramContext(Resource sadResource) {
 		return DUtil.DIAGRAM_CONTEXT_EXPLORER;
 	}
 
+	@Override
+	protected void addPages() {
+		super.addPages();
+
+		final Diagram diagram = this.getDiagramEditor().getDiagramBehavior().getDiagramTypeProvider().getDiagram();
+		ScaModelCommand.execute(diagram, new ScaModelCommand() {
+			@Override
+			public void execute() {
+				diagram.setGridUnit(-1); // hide grid on diagram by setting grid units to -1
+			}
+		});
+	}
 }
