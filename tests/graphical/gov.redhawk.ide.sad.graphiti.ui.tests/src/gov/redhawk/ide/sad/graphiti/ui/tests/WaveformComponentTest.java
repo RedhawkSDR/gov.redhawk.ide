39de9f1589041b8417722138172d0f52d716f3fb
Devin Nelson <devin.nelson@axiosengineering.com>
2015-01-13 17:33:39 -0500

    fixed build path issue

--- tests/graphical/gov.redhawk.ide.sad.graphiti.ui.tests/src/gov/redhawk/ide/sad/graphiti/ui/tests/WaveformComponentTest.java
+++ tests/graphical/gov.redhawk.ide.sad.graphiti.ui.tests/src/gov/redhawk/ide/sad/graphiti/ui/tests/WaveformComponentTest.java
@@ -11,9 +11,9 @@
 package gov.redhawk.ide.sad.graphiti.ui.tests;
 
 import static org.junit.Assert.assertEquals;
-import gov.redhawk.ide.graphiti.ui.diagram.util.DUtil;
 import gov.redhawk.ide.graphiti.sad.ext.impl.ComponentShapeImpl;
 import gov.redhawk.ide.graphiti.sad.ui.diagram.patterns.HostCollocationPattern;
+import gov.redhawk.ide.graphiti.ui.diagram.util.DUtil;
 import gov.redhawk.ide.swtbot.MenuUtils;
 import gov.redhawk.ide.swtbot.WaveformUtils;
 import gov.redhawk.ide.swtbot.diagram.AbstractGraphitiTest;
@@ -21,7 +21,6 @@ import gov.redhawk.ide.swtbot.diagram.ComponentUtils;
 import gov.redhawk.ide.swtbot.diagram.DiagramTestUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
