43e65d3d4086f1ce330354e19bef978deb1b5ede
Neon Ngo <ntn@techma.com>
2015-01-14 14:08:14 -0500

    refactory FindByEditFeature package to be consistent in naming convention,   from gov.redhawk.ide.graphiti.ui.diagram.feature.custom   to   gov.redhawk.ide.graphiti.ui.diagram.features.custom

--- plugins/gov.redhawk.ide.graphiti.ui/src/gov/redhawk/ide/graphiti/ui/diagram/features/custom/FindByEditFeature.java
+++ plugins/gov.redhawk.ide.graphiti.ui/src/gov/redhawk/ide/graphiti/ui/diagram/features/custom/FindByEditFeature.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package gov.redhawk.ide.graphiti.ui.diagram.feature.custom;
+package gov.redhawk.ide.graphiti.ui.diagram.features.custom;
 
 import gov.redhawk.ide.graphiti.ext.impl.RHContainerShapeImpl;
 import gov.redhawk.ide.graphiti.ui.diagram.dialogs.AbstractInputValidationDialog;
