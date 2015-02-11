751cad4650fcadb1de5a5ae671b4a27407a2e9a0
Brad Hoffman <brad@haydensoft.com>
2015-02-11 16:42:19 -0500

    IDE-1026 corrected connections not displaying in scaExplorer

--- plugins/gov.redhawk.ide.graphiti.sad.ui/src-editor/gov/redhawk/ide/graphiti/sad/debug/internal/ui/SadGraphitiModelAdapter.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src-editor/gov/redhawk/ide/graphiti/sad/debug/internal/ui/SadGraphitiModelAdapter.java
@@ -31,33 +31,35 @@ public class SadGraphitiModelAdapter extends EContentAdapter {
 	public void notifyChanged(final Notification notification) {
 		super.notifyChanged(notification);
 		Object newValue = notification.getNewValue();
-		Object notifier = notification.getNotifier();
 		Object oldValue = notification.getOldValue();
 		switch (notification.getEventType()) {
 		case Notification.ADD:
 			if (newValue instanceof SadComponentInstantiation) {
 				final SadComponentInstantiation object = (SadComponentInstantiation) newValue;
 				this.modelMap.add(object);
+			} else if (newValue instanceof SadConnectInterface) {
+				final SadConnectInterface object = (SadConnectInterface) newValue;
+				if (object.getSource() == null || object.getTarget() == null || object.getId() == null) {
+					return;
+				}
+				this.modelMap.add(object);
 			}
-			// Don't listen for sad connection since is isn't complete at this point
 			break;
 		case Notification.ADD_MANY:
 			for (Object obj : ((Collection< ? >) newValue)) {
 				if (obj instanceof SadComponentInstantiation) {
 					final SadComponentInstantiation object = (SadComponentInstantiation) obj;
 					this.modelMap.add(object);
+				} else if (newValue instanceof SadConnectInterface) {
+					final SadConnectInterface object = (SadConnectInterface) newValue;
+					if (object.getSource() == null || object.getTarget() == null || object.getId() == null) {
+						return;
+					}
+					this.modelMap.add(object);
 				}
-				// Don't listen for sad connection since is isn't complete at this point
 			}
 			break;
 		case Notification.SET:
-			if (notifier instanceof SadConnectInterface) {
-				final SadConnectInterface object = (SadConnectInterface) notification.getNotifier();
-				if (object.getSource() == null || object.getTarget() == null || object.getId() == null) {
-					return;
-				}
-				this.modelMap.add(object);
-			}
 			break;
 		case Notification.REMOVE:
 			if (oldValue instanceof SadComponentInstantiation) {
