240f6f53de5299ed822d65edaf2b52952b96330a
Daniel Wille <daniel.wille@axiosengineering.com>
2015-03-26 16:36:49 -0400

    IDE-1182 Updated namespace tests

--- tests/graphical-runtime/gov.redhawk.ide.ui.tests.runtime/src/gov/redhawk/ide/ui/tests/runtime/ScaNamespaceTest.java
+++ tests/graphical-runtime/gov.redhawk.ide.ui.tests.runtime/src/gov/redhawk/ide/ui/tests/runtime/ScaNamespaceTest.java
@@ -27,84 +27,166 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Test;
 
-public class ScaNamespaceTest extends UIRuntimeTest{
+public class ScaNamespaceTest extends UIRuntimeTest {
+
+	private final String prefix = "name.space.";
 
 	SWTBotEditor editor;
 
 	/**
-	 * IDE-1122
-	 * Check that name-spaced objects are properly represented in the SCA Explorer
-	 * Includes: components, devices, services, nodes, and waveforms
+	 * IDE-1122, IDE-1182
+	 * Check that name-spaced component projects can be created, generated and exported.
+	 * They should also be represented in the SCA Explorer.
 	 */
 	@Test
-	public void namespaceBehaviorTest() {
-		final String prefix = "name.space.";
-		final String language = "Python";
-		
+	public void namespaceBehaviorComponents() {
 		final String componentBaseName = "component";
+
+		String projectName = prefix + "cpp." + componentBaseName;
+		ComponentUtils.createComponentProject(bot, projectName, "C++");
+		generateProject(projectName, componentBaseName + ".spd.xml");
+
+		projectName = prefix + "java." + componentBaseName;
+		ComponentUtils.createComponentProject(bot, projectName, "Java");
+		generateProject(projectName, componentBaseName + ".spd.xml");
+
+		projectName = prefix + "python." + componentBaseName;
+		ComponentUtils.createComponentProject(bot, projectName, "Python");
+		generateProject(projectName, componentBaseName + ".spd.xml");
+
+		exportProject(prefix + "cpp." + componentBaseName);
+		exportProject(prefix + "java." + componentBaseName);
+		exportProject(prefix + "python." + componentBaseName);
+
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Components", "name", "space", "cpp" }, componentBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Components", "name", "space", "java" }, componentBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Components", "name", "space", "python" }, componentBaseName);
+	}
+
+	/**
+	 * IDE-1122
+	 * Check that name-spaced device projects can be created, generated and exported.
+	 * They should also be represented in the SCA Explorer.
+	 */
+	@Test
+	public void namespaceBehaviorDevices() {
 		final String deviceBaseName = "device";
+
+		String projectName = prefix + "cpp." + deviceBaseName;
+		DeviceUtils.createDeviceProject(bot, projectName, "C++");
+		generateProject(projectName, deviceBaseName + ".spd.xml");
+
+		projectName = prefix + "java." + deviceBaseName;
+		DeviceUtils.createDeviceProject(bot, projectName, "Java");
+		generateProject(projectName, deviceBaseName + ".spd.xml");
+
+		projectName = prefix + "python." + deviceBaseName;
+		DeviceUtils.createDeviceProject(bot, projectName, "Python");
+		generateProject(projectName, deviceBaseName + ".spd.xml");
+
+		exportProject(prefix + "cpp." + deviceBaseName);
+		exportProject(prefix + "java." + deviceBaseName);
+		exportProject(prefix + "python." + deviceBaseName);
+
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Devices", "name", "space", "cpp" }, deviceBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Devices", "name", "space", "java" }, deviceBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Devices", "name", "space", "python" }, deviceBaseName);
+	}
+
+	/**
+	 * IDE-1122
+	 * Check that a name-spaced service project can be created, generated and exported.
+	 * It should also be represented in the SCA Explorer.
+	 */
+	@Test
+	public void namespaceBehaviorServices() {
 		final String serviceBaseName = "service";
-		final String serviceInterface = "IDL:BULKIO/dataDouble:1.0"; 
-		final String sharedLibraryBaseName = "sharedLibrary";
-		final String sharedLibraryType = "C++ Library";
+		final String serviceInterface = "IDL:BULKIO/dataDouble:1.0";
+
+		String projectName = prefix + "cpp." + serviceBaseName;
+		ServiceUtils.createServiceProject(bot, projectName, serviceInterface, "C++");
+		generateProject(projectName, serviceBaseName + ".spd.xml");
+
+		projectName = prefix + "java." + serviceBaseName;
+		ServiceUtils.createServiceProject(bot, projectName, serviceInterface, "Java");
+		generateProject(projectName, serviceBaseName + ".spd.xml");
+
+		projectName = prefix + "python." + serviceBaseName;
+		ServiceUtils.createServiceProject(bot, projectName, serviceInterface, "Python");
+		generateProject(projectName, serviceBaseName + ".spd.xml");
+
+		exportProject(prefix + "cpp." + serviceBaseName);
+		exportProject(prefix + "java." + serviceBaseName);
+		exportProject(prefix + "python." + serviceBaseName);
+
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Services", "name", "space", "cpp" }, serviceBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Services", "name", "space", "java" }, serviceBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Services", "name", "space", "python" }, serviceBaseName);
+	}
+
+	/**
+	 * IDE-1122
+	 * Check that a name-spaced waveform project can be created and exported.
+	 * It should also be represented in the SCA Explorer.
+	 */
+	@Test
+	public void namespaceBehaviorWaveforms() {
 		final String waveformBaseName = "waveform";
+
+		WaveformUtils.createNewWaveform(bot, prefix + waveformBaseName);
+		exportProject(prefix + waveformBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Waveforms", "name", "space" }, waveformBaseName);
+	}
+
+	/**
+	 * IDE-1122
+	 * Check that a name-spaced service project can be created and exported.
+	 * It should also be represented in the SCA Explorer.
+	 */
+	@Test
+	public void namespaceBehaviorNodes() {
 		final String nodeBaseName = "node";
 		final String nodeDomain = "REDHAWK_DEV";
-		
-		
-		// Create the different project types
-		ComponentUtils.createComponentProject(bot, prefix + componentBaseName, language);
-		generateProject(prefix + componentBaseName, componentBaseName + ".spd.xml");
-		
-		DeviceUtils.createDeviceProject(bot, prefix + deviceBaseName, language);
-		generateProject(prefix + deviceBaseName, deviceBaseName + ".spd.xml");
-		
-		ServiceUtils.createServiceProject(bot, prefix + serviceBaseName, serviceInterface, language);
-		generateProject(prefix + serviceBaseName, serviceBaseName + ".spd.xml");
-		
+
+		NodeUtils.createNewNodeProject(bot, prefix + nodeBaseName, nodeDomain);
+		exportProject(prefix + nodeBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Nodes", "name", "space" }, nodeBaseName);
+	}
+
+	/**
+	 * IDE-1122
+	 * Check that a name-spaced shared library can be created, generated and exported.
+	 * It should also be represented in the SCA Explorer.
+	 */
+	@Test
+	public void namespaceBehaviorSharedLib() {
+		final String sharedLibraryBaseName = "sharedLibrary";
+		final String sharedLibraryType = "C++ Library";
+
 		SharedLibraryUtils.createSharedLibraryProject(bot, prefix + sharedLibraryBaseName, sharedLibraryType);
 		generateProject(prefix + sharedLibraryBaseName, sharedLibraryBaseName + ".spd.xml");
-		
-		WaveformUtils.createNewWaveform(bot, prefix + waveformBaseName);
-		
-		NodeUtils.createNewNodeProject(bot, prefix + nodeBaseName, nodeDomain);
-		
-		// Export all of the projects to the SCA Explorer
-		exportProject(prefix + componentBaseName);
-		exportProject(prefix + deviceBaseName);
-		exportProject(prefix + serviceBaseName);
 		exportProject(prefix + sharedLibraryBaseName);
-		exportProject(prefix + waveformBaseName);
-		exportProject(prefix + nodeBaseName);
-		
-		// Check that each project exists, as well as all containers
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Components", "name", "space"}, componentBaseName);
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Devices", "name", "space"}, deviceBaseName);
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Services", "name", "space"}, serviceBaseName);
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Shared Libraries", "name", "space"}, sharedLibraryBaseName);
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Waveforms", "name", "space"}, waveformBaseName);
-		checkExistsInScaAndRemove(new String[]{"Target SDR", "Nodes", "name", "space"}, nodeBaseName);
+		checkExistsInScaAndRemove(new String[] { "Target SDR", "Shared Libraries", "name", "space" }, sharedLibraryBaseName);
 	}
-	
+
 	private void generateProject(String projectName, String fileName) {
 		ProjectExplorerUtils.openProjectInEditor(bot, new String[] { projectName, fileName });
 		editor = bot.editorByTitle(projectName);
 		StandardTestActions.generateProject(bot, editor);
 		bot.closeAllEditors();
 	}
-	
+
 	private void exportProject(String projectName) {
 		SWTBotTreeItem projectNode = ProjectExplorerUtils.selectNode(bot, projectName);
 		projectNode.contextMenu("Export to SDR").click();
 	}
-	
+
 	private void checkExistsInScaAndRemove(String[] scaPath, String projectName) {
 		ScaExplorerTestUtils.waitUntilNodeAppearsInScaExplorer(bot, scaPath, projectName);
 		SWTBotTreeItem scaNode = ScaExplorerTestUtils.getTreeItemFromScaExplorer(bot, scaPath, projectName);
 		SWTBotMenu deleteContext = scaNode.contextMenu("Delete");
 		deleteContext.click();
 		bot.button("Yes").click();
-		SWTBotTreeItem amIHere;
 		ScaExplorerTestUtils.waitUntilNodeRemovedFromScaExplorer(bot, scaPath, projectName);
 	}
 }
