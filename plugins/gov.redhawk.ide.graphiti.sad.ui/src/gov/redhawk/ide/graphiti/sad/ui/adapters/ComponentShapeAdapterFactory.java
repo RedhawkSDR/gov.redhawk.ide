b47e837ea7562e19073ea9f12f7a38f29b5bea2a
Daniel Wille <daniel.wille@axiosengineering.com>
2015-06-04 14:41:52 -0400

    Code refactor

--- plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/ui/adapters/ComponentShapeAdapterFactory.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/ui/adapters/ComponentShapeAdapterFactory.java
@@ -28,38 +28,47 @@ import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
+/**
+ * An adapter from {@link AbstractGraphicalEditPart} to the corresponding {@link ScaComponent}.
+ */
 public class ComponentShapeAdapterFactory implements IAdapterFactory {
 	private static final Class< ? >[] LIST = new Class< ? >[] { ScaComponent.class };
 
 	@Override
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
-		Object object = adaptableObject;
-		if (object instanceof AbstractGraphicalEditPart) {
-			object = ((AbstractGraphicalEditPart) object).getModel();
+		// Get the model object for the edit part
+		if (!(adaptableObject instanceof AbstractGraphicalEditPart)) {
+			return null;
 		}
-		SadComponentInstantiation ci = null;
-		if (object instanceof ComponentShapeImpl) {
-			ci = (SadComponentInstantiation) DUtil.getBusinessObject((ComponentShapeImpl) object);
+		Object modelObject = ((AbstractGraphicalEditPart) adaptableObject).getModel();
+
+		// Get the business object for the model object
+		if (!(modelObject instanceof ComponentShapeImpl)) {
+			return null;
+		}
+		SadComponentInstantiation ci = (SadComponentInstantiation) DUtil.getBusinessObject((ComponentShapeImpl) modelObject);
+
+		if (ci == null || ci.eResource() == null) {
+			return null;
 		}
 
-		if (ci != null && ci.eResource() != null) {
-			if (adapterType.isAssignableFrom(ScaComponent.class)) {
-				final URI uri = ci.eResource().getURI();
-				final Map<String, String> query = QueryParser.parseQuery(uri.query());
-				final String wfRef = query.get(ScaFileSystemConstants.QUERY_PARAM_WF);
-				final ScaWaveform waveform = ScaModelPlugin.getDefault().findEObject(ScaWaveform.class, wfRef);
-				final String myId = ci.getId();
-				if (waveform != null) {
-					for (final ScaComponent component : GraphitiAdapterUtil.safeFetchComponents(waveform)) {
-						final String scaComponentId = component.identifier();
-						if (scaComponentId.startsWith(myId)) {
-							return component;
-						}
+		if (adapterType.isAssignableFrom(ScaComponent.class)) {
+			final URI uri = ci.eResource().getURI();
+			final Map<String, String> query = QueryParser.parseQuery(uri.query());
+			final String wfRef = query.get(ScaFileSystemConstants.QUERY_PARAM_WF);
+			final ScaWaveform waveform = ScaModelPlugin.getDefault().findEObject(ScaWaveform.class, wfRef);
+			final String myId = ci.getId();
+			if (waveform != null) {
+				for (final ScaComponent component : GraphitiAdapterUtil.safeFetchComponents(waveform)) {
+					final String scaComponentId = component.identifier();
+					if (scaComponentId.startsWith(myId)) {
+						return component;
 					}
 				}
 			}
 		}
+
 		return null;
 	}
 
@@ -67,5 +76,4 @@ public class ComponentShapeAdapterFactory implements IAdapterFactory {
 	public Class< ? >[] getAdapterList() {
 		return ComponentShapeAdapterFactory.LIST;
 	}
-
 }
