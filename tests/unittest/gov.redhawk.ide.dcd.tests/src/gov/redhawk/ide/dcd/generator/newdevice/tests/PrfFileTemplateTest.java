1e1963274041d2e7ac4db954c0bc74992ea46023
Daniel Wille <daniel.wille@axiosengineering.com>
2015-06-23 12:24:00 -0400

    IDE-1292 Test

--- tests/unittest/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newdevice/tests/PrfFileTemplateTest.java
+++ tests/unittest/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newdevice/tests/PrfFileTemplateTest.java
@@ -18,12 +18,15 @@ import gov.redhawk.ide.dcd.tests.TestUtils;
 import java.io.File;
 import java.io.IOException;
 
+import mil.jpeojtrs.sca.prf.AbstractProperty;
 import mil.jpeojtrs.sca.prf.PrfPackage;
 import mil.jpeojtrs.sca.prf.Properties;
+import mil.jpeojtrs.sca.prf.PropertyConfigurationType;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.FeatureMap;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -33,8 +36,9 @@ import org.junit.Test;
 public class PrfFileTemplateTest {
 
 	/**
-	 * Tests generating a PRF file
-	 * 
+	 * Tests generating a PRF file with empty args.
+	 * <p/>
+	 * IDE-1292 Also test that no deprecated properties are included.
 	 * @throws IOException
 	 */
 	@Test
@@ -52,11 +56,15 @@ public class PrfFileTemplateTest {
 		final ResourceSet resourceSet = new ResourceSetImpl();
 		final Properties props = Properties.Util.getProperties(resourceSet.getResource(URI.createFileURI(prfFile.toString()), true));
 		Assert.assertNotNull(props);
+
+		// Ensure configure / execparam aren't used
+		for (FeatureMap.Entry propEntry : props.getProperties()) {
+			Assert.assertFalse(((AbstractProperty) propEntry.getValue()).isKind(PropertyConfigurationType.CONFIGURE, PropertyConfigurationType.EXECPARAM));
+		}
 	}
 	
 	/**
-	 * Tests generating a PRF file
-	 * 
+	 * Tests generating a PRF file with null args
 	 * @throws IOException
 	 */
 	@Test
