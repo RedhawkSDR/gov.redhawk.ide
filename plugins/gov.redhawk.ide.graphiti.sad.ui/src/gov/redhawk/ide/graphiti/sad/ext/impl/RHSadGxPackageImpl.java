fb27ad456088cf7c71228f3b8ced16c37a896b4e
Ryan Bauman <ryan.bauman@axiosengineering.com>
2015-07-24 13:30:56 -0400

    Update headers

--- plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/ext/impl/RHSadGxPackageImpl.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/ext/impl/RHSadGxPackageImpl.java
@@ -1,4 +1,13 @@
 /**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ *
  */
 package gov.redhawk.ide.graphiti.sad.ext.impl;
 
@@ -146,7 +155,7 @@ public class RHSadGxPackageImpl extends EPackageImpl implements RHSadGxPackage {
 
 	/**
 	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
-	 * 
+	 *
 	 * <p>This method is used to initialize {@link RHSadGxPackage#eINSTANCE} when that field is accessed.
 	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
 	 * <!-- begin-user-doc -->
