8ba2fe9195caeaa8c60d562c1a2dfe80154c5572
Daniel Wille <daniel.wille@axiosengineering.com>
2016-03-31 13:33:51 -0400

    Move test to more appropriate package

--- tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/util/tests/AbstractEnvMapTest.java
+++ tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/util/tests/AbstractEnvMapTest.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package gov.redhawk.ide.sdr.internal.util;
+package gov.redhawk.ide.sdr.util.tests;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
