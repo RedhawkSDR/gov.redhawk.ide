5caecc515970450fdf85d0fa10e5f941a4ff12c1
Daniel Wille <daniel.wille@axiosengineering.com>
2016-08-11 13:47:16 -0400

    Make sure test doesn't use the system IDL library

--- tests/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newservice/tests/ScdFileTemplateTest.java
+++ tests/gov.redhawk.ide.dcd.tests/src/gov/redhawk/ide/dcd/generator/newservice/tests/ScdFileTemplateTest.java
@@ -15,12 +15,10 @@ import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import gov.redhawk.eclipsecorba.idl.IdlFactory;
@@ -32,7 +30,6 @@ import gov.redhawk.eclipsecorba.library.RepositoryModule;
 import gov.redhawk.ide.dcd.generator.newservice.GeneratorArgs;
 import gov.redhawk.ide.dcd.generator.newservice.ScdFileTemplate;
 import gov.redhawk.ide.dcd.tests.TestUtils;
-import gov.redhawk.ide.sdr.ui.SdrUiPlugin;
 import gov.redhawk.model.sca.commands.ScaModelCommand;
 import mil.jpeojtrs.sca.scd.Interface;
 import mil.jpeojtrs.sca.scd.ScdPackage;
@@ -44,13 +41,6 @@ import mil.jpeojtrs.sca.util.DceUuidUtil;
  */
 public class ScdFileTemplateTest {
 
-	private IdlLibrary library;
-
-	@Before
-	public void before() {
-		library = SdrUiPlugin.getDefault().getTargetSdrRoot().getIdlLibrary();
-	}
-
 	/**
 	 * Tests generating an SCD file for an IDL the IDE doesn't know about
 	 * @throws IOException
@@ -58,13 +48,15 @@ public class ScdFileTemplateTest {
 	 */
 	@Test
 	public void testCreateServiceSCDFile_UnknownIDL() throws IOException, CoreException {
+		IdlLibrary emptyLibrary = LibraryFactory.eINSTANCE.createIdlLibrary();
+
 		// Generate XML using the template
 		final ScdFileTemplate scdTemplate = new ScdFileTemplate();
 		final GeneratorArgs args = new GeneratorArgs();
 		args.setProjectName("project");
 		args.setSoftPkgId(DceUuidUtil.createDceUUID());
 		args.setAuthorName("Doe");
-		args.setLibrary(library);
+		args.setLibrary(emptyLibrary);
 		args.setRepId("IDL:FOO/FooService:1.0");
 		final String scdContent = scdTemplate.generate(args);
 
@@ -93,15 +85,15 @@ public class ScdFileTemplateTest {
 	@Test
 	public void testCreateServiceSCDFile_IDLWithInheritance() throws IOException, CoreException {
 		// Wait for the IDL library to load
-		library.load(new NullProgressMonitor());
+		final IdlLibrary barLibrary = LibraryFactory.eINSTANCE.createIdlLibrary();
 
 		// Add test interfaces to the IDL library
-		ScaModelCommand.execute(library, new ScaModelCommand() {
+		ScaModelCommand.execute(barLibrary, new ScaModelCommand() {
 			@Override
 			public void execute() {
 				RepositoryModule barRepoModule = LibraryFactory.eINSTANCE.createRepositoryModule();
 				barRepoModule.setName("BAR");
-				library.getDefinitions().add(barRepoModule);
+				barLibrary.getDefinitions().add(barRepoModule);
 
 				Module barModule = IdlFactory.eINSTANCE.createModule();
 				barModule.setName("BAR");
@@ -124,7 +116,7 @@ public class ScdFileTemplateTest {
 		args.setProjectName("project");
 		args.setSoftPkgId(DceUuidUtil.createDceUUID());
 		args.setAuthorName("Doe");
-		args.setLibrary(library);
+		args.setLibrary(barLibrary);
 		args.setRepId("IDL:BAR/FooBarA:1.0");
 		final String scdContent = scdTemplate.generate(args);
 
