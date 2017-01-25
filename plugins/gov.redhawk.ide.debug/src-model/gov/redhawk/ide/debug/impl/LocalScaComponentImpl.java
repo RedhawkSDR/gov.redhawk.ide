38d047eeb6ece44dc6810717e5e364e0fc6644f7
Devin Nelson <dnelson2@lgsinnovations.com>
2017-01-25 15:44:49 -0500

    IDE-1818 - components contained by a component host should not launch a TerminateJob on release.  Otherwise, they will kill the component host and all sibling components

--- plugins/gov.redhawk.ide.debug/src-model/gov/redhawk/ide/debug/impl/LocalScaComponentImpl.java
+++ plugins/gov.redhawk.ide.debug/src-model/gov/redhawk/ide/debug/impl/LocalScaComponentImpl.java
@@ -11,19 +11,27 @@
 // BEGIN GENERATED CODE
 package gov.redhawk.ide.debug.impl;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.resource.Resource;
 
 import CF.LifeCyclePackage.ReleaseError;
 import gov.redhawk.ide.debug.LocalAbstractComponent;
 import gov.redhawk.ide.debug.LocalLaunch;
 import gov.redhawk.ide.debug.LocalScaComponent;
 import gov.redhawk.ide.debug.ScaDebugPackage;
+import gov.redhawk.ide.debug.internal.ComponentLaunch;
 import gov.redhawk.ide.debug.internal.jobs.TerminateJob;
 import gov.redhawk.model.sca.impl.ScaComponentImpl;
+import gov.redhawk.sca.util.SilentJob;
+import mil.jpeojtrs.sca.spd.CodeFileType;
+import mil.jpeojtrs.sca.spd.SoftPkg;
 
 /**
  * <!-- begin-user-doc -->
@@ -407,10 +415,29 @@ public class LocalScaComponentImpl extends ScaComponentImpl implements LocalScaC
 		// END GENERATED CODE
 		// If we have a launch object (i.e. this IDE launched the object locally)
 		if (getLaunch() != null) {
-			Job terminateJob = new TerminateJob(getLaunch(), getName());
-			terminateJob.setUser(false);
-			terminateJob.setSystem(true);
-			terminateJob.schedule();
+			if (SoftPkg.Util.isContainedComponent(getProfileObj())) {
+				Job job = new SilentJob("Local Component Release job") {
+
+					@Override
+					protected IStatus runSilent(IProgressMonitor monitor) {
+						try {
+							releaseObject();
+						} catch (final ReleaseError e) {
+							// PASS
+						}
+						return Status.OK_STATUS;
+					}
+
+				};
+				job.setUser(false);
+				job.setSystem(true);
+				job.schedule();
+			} else {
+				Job terminateJob = new TerminateJob(getLaunch(), getName());
+				terminateJob.setUser(false);
+				terminateJob.setSystem(true);
+				terminateJob.schedule();
+			}
 		}
 
 		super.dispose();
@@ -424,6 +451,10 @@ public class LocalScaComponentImpl extends ScaComponentImpl implements LocalScaC
 		final ILaunch tmpLaunch = getLaunch();
 
 		super.releaseObject();
+
+		if (SoftPkg.Util.isContainedComponent(getProfileObj())) {
+			return;
+		}
 
 		// If it's a local launch, schedule termination after a few seconds to ensure it cleans up
 		if (tmpLaunch != null) {
