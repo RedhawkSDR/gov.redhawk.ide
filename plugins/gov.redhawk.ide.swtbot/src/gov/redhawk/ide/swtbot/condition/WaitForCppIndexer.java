73eaf666ff2797a781027b6e848b7739a59d20e3
Daniel Wille <DWille@lgsinnovations.com>
2017-06-12 18:12:28 -0400

    Add debug msg for our wait condition

--- plugins/gov.redhawk.ide.swtbot/src/gov/redhawk/ide/swtbot/condition/WaitForCppIndexer.java
+++ plugins/gov.redhawk.ide.swtbot/src/gov/redhawk/ide/swtbot/condition/WaitForCppIndexer.java
@@ -10,8 +10,14 @@
  */
 package gov.redhawk.ide.swtbot.condition;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+import org.eclipse.ui.statushandlers.StatusManager;
+
+import gov.redhawk.ide.swtbot.SwtBotActivator;
 
 /**
  * Waits for the C/C++ index to finish. If the indexer is not running, the condition is satisfied.
@@ -20,13 +26,31 @@ public class WaitForCppIndexer extends DefaultCondition {
 
 	public static final long TIMEOUT = 60000;
 
+	private long startingWaitTime;
+	private boolean firstTime;
+
+	@Override
+	public void init(SWTBot bot) {
+		super.init(bot);
+		startingWaitTime = System.currentTimeMillis();
+		firstTime = true;
+	}
+
 	@Override
 	public boolean test() throws Exception {
 		Job[] jobs = Job.getJobManager().find(null);
 		for (Job job : jobs) {
 			if ("org.eclipse.cdt.internal.core.pdom.PDOMIndexerJob".equals(job.getClass().getName())) {
+				firstTime = false;
 				return false;
 			}
+		}
+
+		if (firstTime) {
+			StatusManager.getManager().handle(new Status(IStatus.INFO, SwtBotActivator.PLUGIN_ID, "CDT indexer was not running"), StatusManager.LOG);
+		} else {
+			String msg = String.format("CDT indexer completed in %f seconds", (System.currentTimeMillis() - startingWaitTime) / 1000.0);
+			StatusManager.getManager().handle(new Status(IStatus.INFO, SwtBotActivator.PLUGIN_ID, msg), StatusManager.LOG);
 		}
 		return true;
 	}
