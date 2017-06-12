f2f7d6d50fd171d6b24683fd8e85a3fbfef343a7
Devin Nelson <dnelson2@lgsinnovations.com>
2017-06-12 15:27:39 -0400

    IDE-1941 - Edit support for SAD loggingconfig element

--- plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/internal/ui/editor/pages/ComponentsDetailsPage.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/internal/ui/editor/pages/ComponentsDetailsPage.java
@@ -16,8 +16,19 @@ import java.util.List;
 
 import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.conversion.Converter;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.databinding.EMFUpdateValueStrategy;
+import org.eclipse.emf.databinding.FeaturePath;
+import org.eclipse.emf.databinding.edit.EMFEditObservables;
+import org.eclipse.emf.databinding.edit.EMFEditProperties;
+import org.eclipse.emf.databinding.edit.IEMFEditValueProperty;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
@@ -25,12 +36,15 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
 import gov.redhawk.common.ui.editor.FormLayoutFactory;
+import gov.redhawk.ui.editor.SCAFormEditor;
 import gov.redhawk.ui.editor.ScaDetails;
 import gov.redhawk.ui.parts.FormEntryBindingFactory;
 import gov.redhawk.ui.util.EMFEmptyStringToNullUpdateValueStrategy;
 import gov.redhawk.ui.util.SCAEditorUtil;
 import mil.jpeojtrs.sca.partitioning.ComponentPlacement;
+import mil.jpeojtrs.sca.partitioning.LoggingConfig;
 import mil.jpeojtrs.sca.partitioning.NamingService;
+import mil.jpeojtrs.sca.partitioning.PartitioningFactory;
 import mil.jpeojtrs.sca.partitioning.PartitioningPackage;
 import mil.jpeojtrs.sca.sad.SadComponentInstantiation;
 import mil.jpeojtrs.sca.sad.SoftwareAssembly;
@@ -40,6 +54,28 @@ import mil.jpeojtrs.sca.util.ScaEcoreUtils;
 public class ComponentsDetailsPage extends ScaDetails {
 
 	private SadComponentComposite componentComposite;
+	private Binding loggingUriBinding;
+	private Binding logLevelBinding;
+
+	private final SelectionListener loggingListener = new SelectionAdapter() {
+		public void widgetSelected(final SelectionEvent e) {
+			if (!ComponentsDetailsPage.this.loggingUriBinding.isDisposed()) {
+				Binding binding = ComponentsDetailsPage.this.loggingUriBinding;
+				binding.updateModelToTarget();
+				binding.updateTargetToModel();
+				binding.validateModelToTarget();
+				binding.validateTargetToModel();
+			}
+			if (!ComponentsDetailsPage.this.logLevelBinding.isDisposed()) {
+				Binding binding = ComponentsDetailsPage.this.logLevelBinding;
+				binding.updateModelToTarget();
+				binding.updateTargetToModel();
+				binding.validateModelToTarget();
+				binding.validateTargetToModel();
+			}
+		};
+
+	};
 
 	public ComponentsDetailsPage(final ComponentsSection section) {
 		super(section.getPage());
@@ -77,11 +113,6 @@ public class ComponentsDetailsPage extends ScaDetails {
 
 		SadComponentInstantiation compInst = (SadComponentInstantiation) input;
 
-		NamingService namingService = null;
-		if (compInst.getFindComponent() != null) {
-			namingService = compInst.getFindComponent().getNamingService();
-		}
-
 		SoftPkg softPkg = null;
 		ComponentPlacement< ? > placement = compInst.getPlacement();
 		if (placement.getComponentFileRef() != null && placement.getComponentFileRef().getFile() != null) {
@@ -91,17 +122,17 @@ public class ComponentsDetailsPage extends ScaDetails {
 			return Collections.emptyList();
 		}
 
+		final List<Binding> retVal = new ArrayList<>();
+
+		/** Bind Component Instantiation -> ID **/
+		retVal.add(FormEntryBindingFactory.bind(context, this.componentComposite.getIdEntry(), getEditingDomain(),
+			PartitioningPackage.Literals.COMPONENT_INSTANTIATION__ID, compInst, new EMFEmptyStringToNullUpdateValueStrategy(), null));
+
+		/** Bind Assembly Controller -> RefID (if applicable) **/
 		boolean isAssemblyController = false;
 		if (SoftwareAssembly.Util.isAssemblyController(compInst)) {
 			isAssemblyController = true;
 		}
-
-		final List<Binding> retVal = new ArrayList<>();
-
-		// If we change the component ID, and that component is the assembly controller, we need to update that as well
-		retVal.add(FormEntryBindingFactory.bind(context, this.componentComposite.getIdEntry(), getEditingDomain(),
-			PartitioningPackage.Literals.COMPONENT_INSTANTIATION__ID, compInst, new EMFEmptyStringToNullUpdateValueStrategy(), null));
-
 		if (isAssemblyController) {
 			SoftwareAssembly sad = ScaEcoreUtils.getEContainerOfType(compInst, SoftwareAssembly.class);
 			retVal.add(FormEntryBindingFactory.bind(context, this.componentComposite.getIdEntry(), getEditingDomain(),
@@ -109,16 +140,93 @@ public class ComponentsDetailsPage extends ScaDetails {
 				new EMFEmptyStringToNullUpdateValueStrategy(), null));
 		}
 
-		// If we change the usage name, also update the naming service to match
+		/** Bind Component Instantiation -> Usage Name **/
 		retVal.add(FormEntryBindingFactory.bind(context, this.componentComposite.getNameEntry(), getEditingDomain(),
 			PartitioningPackage.Literals.COMPONENT_INSTANTIATION__USAGE_NAME, compInst, new EMFEmptyStringToNullUpdateValueStrategy(), null));
 
+		/** Bind Component Instantiation -> Find Component -> Naming Service **/
+		NamingService namingService = null;
+		if (compInst.getFindComponent() != null) {
+			namingService = compInst.getFindComponent().getNamingService();
+		}
 		if (namingService != null) {
 			retVal.add(FormEntryBindingFactory.bind(context, this.componentComposite.getNameEntry(), getEditingDomain(),
 				PartitioningPackage.Literals.NAMING_SERVICE__NAME, namingService, new EMFEmptyStringToNullUpdateValueStrategy(), null));
 		}
 
+		/** Bind Component Instantiation -> Logging Config -> Logging URI **/
+		IEMFEditValueProperty uriProperty = EMFEditProperties.value(getEditingDomain(),
+			FeaturePath.fromList(PartitioningPackage.Literals.COMPONENT_INSTANTIATION__LOGGING_CONFIG, PartitioningPackage.Literals.LOGGING_CONFIG__URI));
+		@SuppressWarnings("unchecked")
+		IObservableValue< ? > uriObserver = uriProperty.observe(compInst);
+		this.loggingUriBinding = context.bindValue(
+			WidgetProperties.text(SWT.Modify).observeDelayed(SCAFormEditor.getFieldBindingDelay(), this.componentComposite.getLoggingUri().getText()),
+			uriObserver, null, null);
+		retVal.add(loggingUriBinding);
+
+		/** Bind Component Instantiation -> Logging Config -> Log Level **/
+		IEMFEditValueProperty levelProperty = EMFEditProperties.value(getEditingDomain(),
+			FeaturePath.fromList(PartitioningPackage.Literals.COMPONENT_INSTANTIATION__LOGGING_CONFIG, PartitioningPackage.Literals.LOGGING_CONFIG__LEVEL));
+		@SuppressWarnings("unchecked")
+		IObservableValue< ? > levelObserver = levelProperty.observe(compInst);
+		EMFUpdateValueStrategy levelModelToTarget = new EMFUpdateValueStrategy();
+		levelModelToTarget.setConverter(new Converter(String.class, String.class) {
+			@Override
+			public Object convert(Object fromObject) {
+				if (fromObject == null) {
+					ComponentsDetailsPage.this.componentComposite.getLevelViewer().getCombo().deselectAll();
+				}
+				return fromObject;
+			}
+		});
+		this.logLevelBinding = context.bindValue(WidgetProperties.selection().observe(this.componentComposite.getLevelViewer().getCombo()), levelObserver,
+			new EMFEmptyStringToNullUpdateValueStrategy(), levelModelToTarget);
+		retVal.add(logLevelBinding);
+
+		/** Bind Logging Config enabled button to create/remove logging configuration tags **/
+		final EMFUpdateValueStrategy targetToModel = new EMFUpdateValueStrategy();
+		targetToModel.setConverter(new Converter(Boolean.class, LoggingConfig.class) {
+			@Override
+			public Object convert(final Object fromObject) {
+				if ((Boolean) fromObject) {
+					LoggingConfig loggingConf = PartitioningFactory.eINSTANCE.createLoggingConfig();
+					loggingConf.setUri("");
+					return loggingConf;
+				} else {
+					return null;
+				}
+			}
+		});
+		final EMFUpdateValueStrategy modelToTarget = new EMFUpdateValueStrategy();
+		modelToTarget.setConverter(new Converter(LoggingConfig.class, Boolean.class) {
+
+			@Override
+			public Object convert(final Object fromObject) {
+				return fromObject != null;
+			}
+
+		});
+		retVal.add(context.bindValue(WidgetProperties.selection().observe(this.componentComposite.getEnableLoggingButton()),
+			EMFEditObservables.observeValue(getEditingDomain(), compInst, PartitioningPackage.Literals.COMPONENT_INSTANTIATION__LOGGING_CONFIG), targetToModel,
+			modelToTarget));
+
+		/** Bind Logging Config enabled button to enable/disable Log Level combo and Logging URI text **/
+		retVal.add(context.bindValue(WidgetProperties.enabled().observe(componentComposite.getLoggingUri().getText()),
+			WidgetProperties.selection().observe(componentComposite.getEnableLoggingButton())));
+		retVal.add(context.bindValue(WidgetProperties.enabled().observe(componentComposite.getLevelViewer().getCombo()),
+			WidgetProperties.selection().observe(componentComposite.getEnableLoggingButton())));
+
+		/** Add listener for EnableLoggingButton **/
+		// NOTE: MUST remove listener prior to adding since
+		// 1.) We don't want multiple listeners
+		// 2.) This listener MUST be triggered AFTER the binding listener, therefore, it is adding every time in the
+		// bind method
+		this.componentComposite.getEnableLoggingButton().removeSelectionListener(this.loggingListener);
+		this.componentComposite.getEnableLoggingButton().addSelectionListener(this.loggingListener);
+
+		/** Make all composite fields editable/un-editable depending on the context **/
 		this.componentComposite.setEditable(SCAEditorUtil.isEditableResource(getPage(), compInst.eResource()));
+
 		return retVal;
 	}
 
--- plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/internal/ui/editor/pages/SadComponentComposite.java
+++ plugins/gov.redhawk.ide.graphiti.sad.ui/src/gov/redhawk/ide/graphiti/sad/internal/ui/editor/pages/SadComponentComposite.java
@@ -10,13 +10,26 @@
  *******************************************************************************/
 package gov.redhawk.ide.graphiti.sad.internal.ui.editor.pages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.forms.IFormColors;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 import gov.redhawk.common.ui.editor.FormLayoutFactory;
 import gov.redhawk.common.ui.parts.FormEntry;
+import gov.redhawk.logging.ui.LogLevels;
 import gov.redhawk.ui.editor.IScaComposite;
 
 public class SadComponentComposite extends Composite implements IScaComposite {
@@ -25,20 +38,71 @@ public class SadComponentComposite extends Composite implements IScaComposite {
 	private final FormToolkit toolkit;
 	private FormEntry idEntry;
 	private FormEntry nameEntry;
+	private FormEntry loggingUri;
+	private ComboViewer levelViewer;
 
-	public SadComponentComposite(final Composite parent, final int style, final FormToolkit toolkit,
-		final IEditingDomainProvider editingDomainProvider) {
+	private Button enableLoggingButton;
+
+	public SadComponentComposite(final Composite parent, final int style, final FormToolkit toolkit, final IEditingDomainProvider editingDomainProvider) {
 		super(parent, style);
 
 		this.toolkit = toolkit;
 		setLayout(FormLayoutFactory.createSectionClientGridLayout(false, SadComponentComposite.NUM_COLUMNS));
 
-		this.idEntry = new FormEntry(this, this.toolkit, "Component ID:", SWT.SINGLE);
-
-		this.nameEntry = new FormEntry(this, this.toolkit, "Usage Name:", SWT.SINGLE);
-		this.nameEntry.getText().setToolTipText("Human readable name for the component instantiation");
+		createIdEntry();
+		createNameEntry();
+		createLoggingConfigEntry();
 
 		toolkit.paintBordersFor(this);
+	}
+
+	private void createLoggingConfigEntry() {
+		// Logging Config
+		Label loggingConfigLabel = toolkit.createLabel(this, "Logging Configuration:");
+		loggingConfigLabel.setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
+		loggingConfigLabel.setLayoutData(GridDataFactory.fillDefaults().span(1, 1).create());
+		this.enableLoggingButton = toolkit.createButton(this, "Enable", SWT.CHECK);
+		this.enableLoggingButton.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).create());
+
+		toolkit.createLabel(this, "");
+		Composite loggingComposite = toolkit.createComposite(this);
+		loggingComposite.setLayout(FormLayoutFactory.createSectionClientGridLayout(false, 3));
+		loggingComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		// Log Level
+		Label levelLabel = toolkit.createLabel(loggingComposite, "Log Level:");
+		levelLabel.setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
+		levelLabel.setLayoutData(GridDataFactory.fillDefaults().create());
+		this.levelViewer = new ComboViewer(loggingComposite, SWT.DROP_DOWN | SWT.READ_ONLY);
+		toolkit.adapt(this.levelViewer.getCombo());
+		this.levelViewer.getControl().setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, false).create());
+		this.levelViewer.setContentProvider(new ArrayContentProvider());
+		this.levelViewer.setLabelProvider(new LabelProvider());
+
+		List<Object> logLevels = new ArrayList<>();
+		logLevels.addAll(Arrays.asList(LogLevels.values()));
+		logLevels.add(0, ""); // Add blank entry to allow unsetting log level
+		this.levelViewer.setInput(logLevels);
+
+		// Logging URI
+		this.loggingUri = new FormEntry(loggingComposite, this.toolkit, "Logging URI:", SWT.SINGLE);
+
+		toolkit.paintBordersFor(loggingComposite);
+	}
+
+	/**
+	 * Text field for Component ID
+	 */
+	private void createIdEntry() {
+		this.idEntry = new FormEntry(this, this.toolkit, "Component ID:", SWT.SINGLE);
+	}
+
+	/**
+	 * Text field for Component Usage Name / Naming Service Name (edits here change both)
+	 */
+	private void createNameEntry() {
+		this.nameEntry = new FormEntry(this, this.toolkit, "Usage Name:", SWT.SINGLE);
+		this.nameEntry.getText().setToolTipText("Human readable name for the component instantiation");
 	}
 
 	public FormEntry getIdEntry() {
@@ -47,6 +111,18 @@ public class SadComponentComposite extends Composite implements IScaComposite {
 
 	public FormEntry getNameEntry() {
 		return this.nameEntry;
+	}
+
+	public FormEntry getLoggingUri() {
+		return this.loggingUri;
+	}
+
+	public ComboViewer getLevelViewer() {
+		return this.levelViewer;
+	}
+
+	public Button getEnableLoggingButton() {
+		return this.enableLoggingButton;
 	}
 
 	@Override
