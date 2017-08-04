5113f044475d1f50638a9728667ebdd86bb79c96
Daniel Wille <DWille@lgsinnovations.com>
2017-08-04 10:46:11 -0400

    Deprecate a test method

--- plugins/gov.redhawk.ide.swtbot/src/gov/redhawk/ide/swtbot/diagram/DiagramTestUtils.java
+++ plugins/gov.redhawk.ide.swtbot/src/gov/redhawk/ide/swtbot/diagram/DiagramTestUtils.java
@@ -225,8 +225,9 @@ public class DiagramTestUtils {
 	}
 
 	/**
-	 * Add a Use FrontEnd Tuner Device to the SAD diagram editor
+	 * @deprecated Use {@link #addUseFrontEndTunerDeviceToDiagram(SWTGefBot, RHBotGefEditor, int, int)}
 	 */
+	@Deprecated
 	public static void addUseFrontEndTunerDeviceToDiagram(SWTGefBot gefBot, RHBotGefEditor editor) {
 		addUseFrontEndTunerDeviceToDiagram(gefBot, editor, 0, 0);
 	}
