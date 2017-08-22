530b4008d931c4bbe3fa1c5f8a0ab246e64dd0d7
Daniel Wille <DWille@lgsinnovations.com>
2017-08-22 14:43:41 -0400

    Change package for some internal classes

--- plugins/gov.redhawk.ide.sdr.ui/src/gov/redhawk/ide/sdr/internal/ui/properties/SdrPropertiesProvider.java
+++ plugins/gov.redhawk.ide.sdr.ui/src/gov/redhawk/ide/sdr/internal/ui/properties/SdrPropertiesProvider.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package gov.redhawk.ide.sdr.internal.ui;
+package gov.redhawk.ide.sdr.internal.ui.properties;
 
 import gov.redhawk.ide.sdr.SdrRoot;
 import gov.redhawk.ide.sdr.ui.SdrUiPlugin;
