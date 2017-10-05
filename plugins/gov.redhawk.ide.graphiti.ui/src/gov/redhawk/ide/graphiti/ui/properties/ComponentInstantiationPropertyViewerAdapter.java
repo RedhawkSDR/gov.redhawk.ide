d14ff00f8aac25cdfc453cc078bd93358989a51c
Devin Nelson <dnelson2@lgsinnovations.com>
2017-10-05 16:04:18 -0400

    IDE-1502 - added null check

--- plugins/gov.redhawk.ide.graphiti.ui/src/gov/redhawk/ide/graphiti/ui/properties/ComponentInstantiationPropertyViewerAdapter.java
+++ plugins/gov.redhawk.ide.graphiti.ui/src/gov/redhawk/ide/graphiti/ui/properties/ComponentInstantiationPropertyViewerAdapter.java
@@ -134,7 +134,10 @@ public class ComponentInstantiationPropertyViewerAdapter {
 			component.unsetProfileObj();
 		}
 		ignore = false;
-		viewer.refresh();
+
+		if (viewer != null) {
+			viewer.refresh();
+		}
 	}
 
 	public void dispose() {
