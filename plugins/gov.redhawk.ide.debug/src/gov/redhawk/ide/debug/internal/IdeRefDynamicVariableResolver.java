88f211e1d3027a38911c6dd79f0d50a61e234a18
Daniel Wille <DWille@lgsinnovations.com>
2018-04-16 17:04:02 -0400

    Don't toString() for IOR
    The IOR should already be available in getIor().

--- plugins/gov.redhawk.ide.debug/src/gov/redhawk/ide/debug/internal/IdeRefDynamicVariableResolver.java
+++ plugins/gov.redhawk.ide.debug/src/gov/redhawk/ide/debug/internal/IdeRefDynamicVariableResolver.java
@@ -10,24 +10,19 @@
  *******************************************************************************/
 package gov.redhawk.ide.debug.internal;
 
-
-import gov.redhawk.ide.debug.ScaDebugPlugin;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.variables.IDynamicVariable;
 import org.eclipse.core.variables.IDynamicVariableResolver;
 
-import ExtendedCF.Sandbox;
+import gov.redhawk.ide.debug.LocalSca;
+import gov.redhawk.ide.debug.ScaDebugPlugin;
+
 public class IdeRefDynamicVariableResolver implements IDynamicVariableResolver {
 
 	@Override
 	public String resolveValue(IDynamicVariable variable, String argument) throws CoreException {
-		Sandbox sandbox = ScaDebugPlugin.getInstance().getSandbox();
-		if (sandbox != null) {
-			return sandbox.toString();
-		} else {
-			return null;
-		}
+		LocalSca localSca = ScaDebugPlugin.getInstance().getLocalSca();
+		return (localSca != null) ? localSca.getIor() : null;
 	}
 
 }
