5d8de4366616321912d6f2bcff755bd67c895e9e
Daniel Wille <DWille@lgsinnovations.com>
2018-05-07 12:50:28 -0400

    IDE-2187 Cleanup shared memory from crashed comps/devs

--- plugins/gov.redhawk.ide.debug.linux/src/gov/redhawk/ide/debug/linux/PosixRuntimeProcess.java
+++ plugins/gov.redhawk.ide.debug.linux/src/gov/redhawk/ide/debug/linux/PosixRuntimeProcess.java
@@ -10,13 +10,19 @@
  *******************************************************************************/
 package gov.redhawk.ide.debug.linux;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.RuntimeProcess;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * 
@@ -39,28 +45,59 @@ public class PosixRuntimeProcess extends RuntimeProcess {
 		if (isTerminated()) {
 			return;
 		}
-		Exception error = null;
+		IOException error = null;
 		try {
-			ProcessTree tree = new ProcessTree();
-			Process p = getSystemProcess();
-			if (p == null) {
-				return;
-			}
-			Class< ? > unixProcessClass = Class.forName("java.lang.UNIXProcess");
-			if (unixProcessClass.isInstance(p)) {
-				Field pidField = p.getClass().getDeclaredField("pid");
-				pidField.setAccessible(true);
-				int pid = pidField.getInt(p);
+			int pid = getPid();
+			if (pid != -1) {
+				ProcessTree tree = new ProcessTree();
 				tree.killAll(pid);
 			}
-		} catch (Exception e) { // SUPPRESS CHECKSTYLE Catch system errors
+		} catch (IOException e) {
 			error = e;
 		}
 		super.terminate();
 		if (error != null) {
-			throw new DebugException(new Status(Status.ERROR, "gov.redhawk.ide.debug.linux", "Failed to terminate all sub processes", error));
+			throw new DebugException(new Status(Status.ERROR, "gov.redhawk.ide.debug.linux", "Failed to terminate all subprocesses", error));
 		}
 
+	}
+
+	@Override
+	protected void terminated() {
+		// C++ components/devices using shared memory for their BulkIO transport can leave orphaned shared memory if
+		// they crash. We remove it if it still exists.
+		int pid = getPid();
+		try {
+			Files.deleteIfExists(Paths.get("/dev/shm/heap-" + pid));
+		} catch (IOException e) {
+			// PASS
+		}
+
+		super.terminated();
+	}
+
+	/**
+	 * @return The PID for the actual system process, or -1 if none
+	 */
+	private int getPid() {
+		try {
+			Process p = getSystemProcess();
+			if (p == null) {
+				return -1;
+			}
+			Class< ? > unixProcessClass = Class.forName("java.lang.UNIXProcess");
+			if (!unixProcessClass.isInstance(p)) {
+				return -1;
+			}
+			Field pidField = p.getClass().getDeclaredField("pid");
+			pidField.setAccessible(true);
+			int pid = pidField.getInt(p);
+			return pid;
+		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			IStatus status = new Status(IStatus.ERROR, "gov.redhawk.ide.debug.linux", "Unable to get process PID", e);
+			Platform.getLog(FrameworkUtil.getBundle(getClass())).log(status);
+			return -1;
+		}
 	}
 
 }
