378b7c16b8b3b76d73ca3c2d5fbd71b96b776175
Daniel Wille <DWille@lgsinnovations.com>
2018-08-16 16:57:30 -0400

    IDE-2222 Test resolving SdrRoot dynamic variable

---
+++ tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/tests/variables/SdrRootResolverTest.java
@@ -0,0 +1,44 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+package gov.redhawk.ide.sdr.tests.variables;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.core.variables.IDynamicVariable;
+import org.eclipse.core.variables.VariablesPlugin;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import gov.redhawk.ide.sdr.IdeSdrActivator;
+import gov.redhawk.ide.sdr.preferences.IdeSdrPreferenceConstants;
+
+public class SdrRootResolverTest {
+
+	private IDynamicVariable variable;
+
+	@Before
+	public void before() throws Exception {
+		variable = VariablesPlugin.getDefault().getStringVariableManager().getDynamicVariable("SdrRoot");
+	}
+
+	@Test
+	public void resolve() throws CoreException {
+		InstanceScope.INSTANCE.getNode(IdeSdrActivator.PLUGIN_ID).put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "/foo");
+		Assert.assertEquals("/foo", variable.getValue(null));
+	}
+
+	@After
+	public void after() throws CoreException {
+		InstanceScope.INSTANCE.getNode(IdeSdrActivator.PLUGIN_ID).remove(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE);
+	}
+}
