3e8463050509004c76afd0a0561d7f6b747f994e
Daniel Wille <DWille@lgsinnovations.com>
2018-08-16 16:44:19 -0400

    IDE-2222 Move SDRROOT dynamic variable resolver

--- plugins/gov.redhawk.ide.sdr/src/gov/redhawk/ide/sdr/variables/SdrRootResolver.java
+++ plugins/gov.redhawk.ide.sdr/src/gov/redhawk/ide/sdr/variables/SdrRootResolver.java
@@ -1,25 +1,25 @@
-/*******************************************************************************
- * This file is protected by Copyright. 
+/**
+ * This file is protected by Copyright.
  * Please refer to the COPYRIGHT file distributed with this source distribution.
  *
  * This file is part of REDHAWK IDE.
  *
- * All rights reserved.  This program and the accompanying materials are made available under 
- * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-package gov.redhawk.ide.sdr.ui.varables;
-
-import gov.redhawk.ide.sdr.preferences.IdeSdrPreferences;
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+package gov.redhawk.ide.sdr.variables;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.variables.IDynamicVariable;
 import org.eclipse.core.variables.IDynamicVariableResolver;
 
+import gov.redhawk.ide.sdr.preferences.IdeSdrPreferences;
+
 /**
  * This resolver provides the REDHAWK IDE preference value for the SDRROOT variable.
  * 
- * @see RedhawkIdeActivator#getRuntimePath()
+ * @see IdeSdrPreferences#getTargetSdrPath()
  */
 public class SdrRootResolver implements IDynamicVariableResolver {
 
