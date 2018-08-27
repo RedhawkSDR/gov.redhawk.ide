ca1245593f9efd8428ad23a3b44d70bc579358d6
Daniel Wille <DWille@lgsinnovations.com>
2018-08-27 11:38:58 -0400

    IDE-2222 Update test for 2.2

--- tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/tests/TargetSDRRootTest.java
+++ tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/tests/TargetSDRRootTest.java
@@ -13,9 +13,8 @@ package gov.redhawk.ide.sdr.tests;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -34,11 +33,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.osgi.framework.FrameworkUtil;
 
+import gov.redhawk.ide.sdr.ComponentsContainer;
+import gov.redhawk.ide.sdr.DevicesContainer;
 import gov.redhawk.ide.sdr.IdeSdrActivator;
 import gov.redhawk.ide.sdr.LoadState;
+import gov.redhawk.ide.sdr.NodesContainer;
 import gov.redhawk.ide.sdr.SdrPackage;
 import gov.redhawk.ide.sdr.SdrRoot;
+import gov.redhawk.ide.sdr.ServicesContainer;
+import gov.redhawk.ide.sdr.SharedLibrariesContainer;
 import gov.redhawk.ide.sdr.TargetSdrRoot;
+import gov.redhawk.ide.sdr.WaveformsContainer;
 import gov.redhawk.ide.sdr.preferences.IdeSdrPreferenceConstants;
 import gov.redhawk.model.sca.commands.ScaModelCommand;
 
@@ -93,37 +98,107 @@ public class TargetSDRRootTest {
 		Assert.assertEquals(LoadState.LOADED, loadStates.poll(10, TimeUnit.SECONDS));
 
 		// Verify the appropriate components, devices, etc are present
-		Set<String> names = sdrRoot.getComponentsContainer().getComponents().stream() //
-				.map(component -> component.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Arrays.asList("CppComponentWithDeps", "CppComponentWithDeps2", "FrequencyShift", "Reader", "Writer"));
-		names = sdrRoot.getSharedLibrariesContainer().getComponents().stream() //
-				.map(sharedlib -> sharedlib.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Arrays.asList("CppDepA", "CppDepAB", "CppDepAC", "CppDepD", "CppDepDE"));
-		names = sdrRoot.getDevicesContainer().getComponents().stream() //
-				.map(device -> device.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Arrays.asList("BasicTestDevice"));
-		names = sdrRoot.getServicesContainer().getComponents().stream() //
-				.map(service -> service.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Collections.emptyList());
-		names = sdrRoot.getWaveformsContainer().getWaveforms().stream() //
-				.map(waveform -> waveform.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Arrays.asList("test"));
-		names = sdrRoot.getNodesContainer().getNodes().stream() //
-				.map(node -> node.getName()) //
-				.collect(Collectors.toSet());
-		assertNames(names, Arrays.asList("DeviceManager"));
+		assertNames(sdrRoot.getComponentsContainer(), new String[0],
+			Arrays.asList("CppComponentWithDeps", "CppComponentWithDeps2", "FrequencyShift", "Reader", "Writer"));
+		assertNames(sdrRoot.getComponentsContainer(), new String[] { "rh" }, Arrays.asList("rh.SigGen"));
+		assertNames(sdrRoot.getSharedLibrariesContainer(), new String[0], Arrays.asList("CppDepA", "CppDepAB", "CppDepAC", "CppDepD", "CppDepDE"));
+		assertNames(sdrRoot.getSharedLibrariesContainer(), new String[] { "rh" }, Arrays.asList("rh.dsp"));
+		assertNames(sdrRoot.getDevicesContainer(), new String[0], Arrays.asList("BasicTestDevice"));
+		assertNames(sdrRoot.getDevicesContainer(), new String[] { "rh" }, Arrays.asList("rh.FmRdsSimulator"));
+		assertNames(sdrRoot.getServicesContainer(), new String[0], Arrays.asList("exampleService1"));
+		assertNames(sdrRoot.getServicesContainer(), new String[] { "name", "space" }, Arrays.asList("name.space.exampleService2"));
+		assertNames(sdrRoot.getWaveformsContainer(), new String[0], Arrays.asList("test"));
+		assertNames(sdrRoot.getWaveformsContainer(), new String[] { "demo" }, Arrays.asList("demo.genwave"));
+		assertNames(sdrRoot.getNodesContainer(), new String[0], Arrays.asList("DeviceManager"));
+		assertNames(sdrRoot.getNodesContainer(), new String[] { "demo" }, Arrays.asList("demo.node"));
 	}
 
-	private void assertNames(Set<String> actualNames, List<String> requiredNames) {
+	private void assertNames(EObject container, String[] path, List<String> requiredNames) {
+		for (String pathElement : path) {
+			Optional< ? extends EObject> childContainer;
+			switch (container.eClass().getClassifierID()) {
+			case SdrPackage.COMPONENTS_CONTAINER:
+				childContainer = ((ComponentsContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			case SdrPackage.DEVICES_CONTAINER:
+				childContainer = ((DevicesContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			case SdrPackage.SERVICES_CONTAINER:
+				childContainer = ((ServicesContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			case SdrPackage.SHARED_LIBRARIES_CONTAINER:
+				childContainer = ((SharedLibrariesContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			case SdrPackage.WAVEFORMS_CONTAINER:
+				childContainer = ((WaveformsContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			case SdrPackage.NODES_CONTAINER:
+				childContainer = ((NodesContainer) container).getChildContainers().stream() //
+						.filter(child -> pathElement.equals(child.getName())) //
+						.findFirst();
+				break;
+			default:
+				Assert.fail("Unknown container type: " + container);
+				return;
+			}
+
+			Assert.assertTrue("Missing child container: " + path, childContainer.isPresent());
+			container = childContainer.get();
+		}
+
+		List<String> actualNames;
+		switch (container.eClass().getClassifierID()) {
+		case SdrPackage.COMPONENTS_CONTAINER:
+			actualNames = ((ComponentsContainer) container).getComponents().stream() //
+					.map(spd -> spd.getName()) //
+					.collect(Collectors.toList());
+			break;
+		case SdrPackage.DEVICES_CONTAINER:
+			actualNames = ((DevicesContainer) container).getComponents().stream() //
+					.map(spd -> spd.getName()) //
+					.collect(Collectors.toList());
+			break;
+		case SdrPackage.SERVICES_CONTAINER:
+			actualNames = ((ServicesContainer) container).getComponents().stream() //
+					.map(spd -> spd.getName()) //
+					.collect(Collectors.toList());
+			break;
+		case SdrPackage.SHARED_LIBRARIES_CONTAINER:
+			actualNames = ((SharedLibrariesContainer) container).getComponents().stream() //
+					.map(spd -> spd.getName()) //
+					.collect(Collectors.toList());
+			break;
+		case SdrPackage.WAVEFORMS_CONTAINER:
+			actualNames = ((WaveformsContainer) container).getWaveforms().stream() //
+					.map(sad -> sad.getName()) //
+					.collect(Collectors.toList());
+			break;
+		case SdrPackage.NODES_CONTAINER:
+			actualNames = ((NodesContainer) container).getNodes().stream() //
+					.map(dcd -> dcd.getName()) //
+					.collect(Collectors.toList());
+			break;
+		default:
+			Assert.fail("Unknown container type: " + container);
+			return;
+		}
+
 		for (String requiredName : requiredNames) {
 			Assert.assertTrue("Did not find " + requiredName, actualNames.remove(requiredName));
 		}
-		Assert.assertTrue(actualNames.isEmpty());
+		if (!actualNames.isEmpty()) {
+			Assert.fail("Names of resources in container that were not expected: " + actualNames);
+		}
 	}
 
 	@After
