3d2e3c87adcba98af8f47e352aafb0347f22fd1c
Daniel Wille <DWille@lgsinnovations.com>
2018-09-10 16:48:00 -0400

    IDE-2222 Expand path check tests

--- tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/tests/preferences/IdeSdrPreferencesTest.java
+++ tests/gov.redhawk.ide.sdr.tests/src/gov/redhawk/ide/sdr/tests/preferences/IdeSdrPreferencesTest.java
@@ -14,7 +14,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.osgi.service.prefs.Preferences;
 
@@ -24,32 +23,36 @@ import gov.redhawk.ide.sdr.preferences.IdeSdrPreferences;
 
 public class IdeSdrPreferencesTest {
 
-	@BeforeClass
-	public static void before() throws CoreException {
-		Preferences prefs = InstanceScope.INSTANCE.getNode(IdeSdrActivator.PLUGIN_ID);
-		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "/foo");
-		prefs.put(IdeSdrPreferenceConstants.TARGET_SDR_DOM_PATH, "a");
-		prefs.put(IdeSdrPreferenceConstants.TARGET_SDR_DEV_PATH, "b");
-	}
+	private static Preferences prefs = InstanceScope.INSTANCE.getNode(IdeSdrActivator.PLUGIN_ID);
 
 	@Test
 	public void getTargetSdrDomPath() {
+		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "/foo");
+		prefs.put(IdeSdrPreferenceConstants.TARGET_SDR_DOM_PATH, "a");
 		Assert.assertEquals("/foo/a", IdeSdrPreferences.getTargetSdrDomPath().toString());
 	}
 
 	@Test
 	public void getTargetSdrDevPath() {
+		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "/foo");
+		prefs.put(IdeSdrPreferenceConstants.TARGET_SDR_DEV_PATH, "b");
 		Assert.assertEquals("/foo/b", IdeSdrPreferences.getTargetSdrDevPath().toString());
 	}
 
 	@Test
 	public void getTargetSdrPath() {
+		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "/foo");
 		Assert.assertEquals("/foo", IdeSdrPreferences.getTargetSdrPath().toString());
+
+		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "${unknown_var}");
+		Assert.assertEquals(null, IdeSdrPreferences.getTargetSdrPath());
+
+		prefs.put(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE, "");
+		Assert.assertEquals(null, IdeSdrPreferences.getTargetSdrPath());
 	}
 
 	@AfterClass
 	public static void after() throws CoreException {
-		Preferences prefs = InstanceScope.INSTANCE.getNode(IdeSdrActivator.PLUGIN_ID);
 		prefs.remove(IdeSdrPreferenceConstants.SCA_LOCAL_SDR_PATH_PREFERENCE);
 		prefs.remove(IdeSdrPreferenceConstants.TARGET_SDR_DOM_PATH);
 		prefs.remove(IdeSdrPreferenceConstants.TARGET_SDR_DEV_PATH);
